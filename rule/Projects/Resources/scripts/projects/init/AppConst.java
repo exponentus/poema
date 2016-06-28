747945cc8b34a796eb8388799649715524fa2d00
k-zon_000 <k-zon_000@KayraNote.lan>
2016-06-29 00:31:24 +0300

    №

--- rule/Projects/Resources/scripts/projects/init/AppConst.java
+++ rule/Projects/Resources/scripts/projects/init/AppConst.java
@@ -6,7 +6,7 @@ public class AppConst {
 	public static String NAME = "Projects";
 	public static String NAME_ENG = "Projects";
 	public static String NAME_RUS = "Проекты";
-	public static String NAME_KAZ = "Проекты";
+	public static String NAME_KAZ = "Проект";
 	public static AppCode CODE = AppCode.CUSTOM;
 	public static String DEFAULT_URL = "index.html";
 	public static String FT_INDEX_SCOPE = "";
