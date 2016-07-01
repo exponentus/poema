6cf6b4b0dcacc28e90d061c51c95b5b6aaea6d57
k-zon_000 <k-zon_000@KayraNote>
2016-07-01 20:00:27 +0300

    #

--- rule/Projects/Resources/scripts/projects/page/form/TaskForm.java
+++ rule/Projects/Resources/scripts/projects/page/form/TaskForm.java
@@ -1,29 +1,5 @@
 package projects.page.form;
 
-import administrator.dao.UserDAO;
-import administrator.model.User;
-import com.exponentus.common.dao.AttachmentDAO;
-import com.exponentus.common.model.Attachment;
-import com.exponentus.env.EnvConst;
-import com.exponentus.env.Environment;
-import com.exponentus.exception.SecureException;
-import com.exponentus.localization.LanguageCode;
-import com.exponentus.scripting.*;
-import com.exponentus.scripting.event._DoForm;
-import com.exponentus.user.IUser;
-import com.exponentus.util.TimeUtil;
-import com.exponentus.webserver.servlet.UploadedFile;
-import org.apache.commons.io.IOUtils;
-import org.eclipse.persistence.exceptions.DatabaseException;
-import projects.dao.ProjectDAO;
-import projects.dao.TaskDAO;
-import projects.model.Task;
-import projects.model.constants.TaskPriorityType;
-import projects.model.constants.TaskStatusType;
-import reference.dao.TagDAO;
-import reference.dao.TaskTypeDAO;
-import reference.model.Tag;
-
 import java.io.File;
 import java.io.FileInputStream;
 import java.io.IOException;
@@ -33,205 +9,232 @@ import java.util.Date;
 import java.util.List;
 import java.util.UUID;
 
+import org.apache.commons.io.IOUtils;
+import org.eclipse.persistence.exceptions.DatabaseException;
+
+import com.exponentus.common.dao.AttachmentDAO;
+import com.exponentus.common.model.Attachment;
+import com.exponentus.env.EnvConst;
+import com.exponentus.env.Environment;
+import com.exponentus.exception.SecureException;
+import com.exponentus.localization.LanguageCode;
+import com.exponentus.scripting.IPOJOObject;
+import com.exponentus.scripting._Exception;
+import com.exponentus.scripting._POJOListWrapper;
+import com.exponentus.scripting._Session;
+import com.exponentus.scripting._Validation;
+import com.exponentus.scripting._WebFormData;
+import com.exponentus.scripting.event._DoForm;
+import com.exponentus.user.IUser;
+import com.exponentus.util.TimeUtil;
+import com.exponentus.webserver.servlet.UploadedFile;
+
+import administrator.dao.UserDAO;
+import administrator.model.User;
+import projects.dao.ProjectDAO;
+import projects.dao.TaskDAO;
+import projects.model.Task;
+import projects.model.constants.TaskPriorityType;
+import projects.model.constants.TaskStatusType;
+import reference.dao.TagDAO;
+import reference.dao.TaskTypeDAO;
+import reference.model.Tag;
+
 public class TaskForm extends _DoForm {
 
-    @SuppressWarnings("unchecked")
-    @Override
-    public void doGET(_Session session, _WebFormData formData) {
-        IUser<Long> user = session.getUser();
-        Task task;
-        String id = formData.getValueSilently("taskId");
+	@SuppressWarnings("unchecked")
+	@Override
+	public void doGET(_Session session, _WebFormData formData) {
+		IUser<Long> user = session.getUser();
+		Task task;
+		String id = formData.getValueSilently("taskId");
 
-        if (!id.isEmpty()) {
-            TaskDAO dao = new TaskDAO(session);
-            task = dao.findById(UUID.fromString(id));
+		if (!id.isEmpty()) {
+			TaskDAO dao = new TaskDAO(session);
+			task = dao.findById(UUID.fromString(id));
 
-            String attachmentId = formData.getValueSilently("attachment");
-            if (!attachmentId.isEmpty()) {
-                AttachmentDAO attachmentDAO = new AttachmentDAO(session);
-                Attachment attachment = attachmentDAO.findById(attachmentId);
-                if (showAttachment(attachment)) {
-                    return;
-                } else {
-                    setBadRequest();
-                }
-            }
-        } else {
-            task = new Task();
-            task.setAuthor(user);
-            task.setRegDate(new Date());
-            TaskTypeDAO tDao = new TaskTypeDAO(session);
-            task.setTaskType(tDao.findByName("Programming"));
-            task.setStartDate(new Date());
-            String fsId = formData.getValueSilently(EnvConst.FSID_FIELD_NAME);
+			String attachmentId = formData.getValueSilently("attachment");
+			if (!attachmentId.isEmpty()) {
+				AttachmentDAO attachmentDAO = new AttachmentDAO(session);
+				Attachment attachment = attachmentDAO.findById(attachmentId);
+				if (showAttachment(attachment)) {
+					return;
+				} else {
+					setBadRequest();
+				}
+			}
+		} else {
+			task = new Task();
+			task.setAuthor(user);
+			TaskTypeDAO tDao = new TaskTypeDAO(session);
+			task.setTaskType(tDao.findByName("Programming"));
+			task.setStartDate(new Date());
+			String fsId = formData.getValueSilently(EnvConst.FSID_FIELD_NAME);
 
-            List<String> formFiles = null;
-            Object obj = session.getAttribute(fsId);
-            if (obj == null) {
-                formFiles = new ArrayList<>();
-            } else {
-                formFiles = (List<String>) obj;
-            }
+			List<String> formFiles = null;
+			Object obj = session.getAttribute(fsId);
+			if (obj == null) {
+				formFiles = new ArrayList<>();
+			} else {
+				formFiles = (List<String>) obj;
+			}
 
-            List<IPOJOObject> filesToPublish = new ArrayList<>();
+			List<IPOJOObject> filesToPublish = new ArrayList<>();
 
-            for (String fn : formFiles) {
-                UploadedFile uf = (UploadedFile) session.getAttribute(fsId + "_file" + fn);
-                if (uf == null) {
-                    uf = new UploadedFile();
-                    uf.setName(fn);
-                    session.setAttribute(fsId + "_file" + fn, uf);
-                }
-                filesToPublish.add(uf);
-            }
-            addContent(new _POJOListWrapper<>(filesToPublish, session));
-        }
+			for (String fn : formFiles) {
+				UploadedFile uf = (UploadedFile) session.getAttribute(fsId + "_file" + fn);
+				if (uf == null) {
+					uf = new UploadedFile();
+					uf.setName(fn);
+					session.setAttribute(fsId + "_file" + fn, uf);
+				}
+				filesToPublish.add(uf);
+			}
+			addContent(new _POJOListWrapper<>(filesToPublish, session));
+		}
 
-        addContent(task);
-        startSaveFormTransact(task);
-    }
+		addContent(task);
+	}
 
-    @Override
-    public void doPOST(_Session session, _WebFormData formData) {
-        try {
-            _Validation ve = validate(formData, session.getLang());
-            if (ve.hasError()) {
-                setBadRequest();
-                setValidation(ve);
-                return;
-            }
+	@Override
+	public void doPOST(_Session session, _WebFormData formData) {
+		try {
+			_Validation ve = validate(formData, session.getLang());
+			if (ve.hasError()) {
+				setBadRequest();
+				setValidation(ve);
+				return;
+			}
 
-            UserDAO userDAO = new UserDAO(session);
-            ProjectDAO projectDAO = new ProjectDAO(session);
-            TaskTypeDAO taskTypeDAO = new TaskTypeDAO(session);
-            TaskDAO dao = new TaskDAO(session);
-            Task task;
-            String id = formData.getValueSilently("taskId");
-            boolean isNew = id.isEmpty();
+			UserDAO userDAO = new UserDAO(session);
+			ProjectDAO projectDAO = new ProjectDAO(session);
+			TaskTypeDAO taskTypeDAO = new TaskTypeDAO(session);
+			TaskDAO dao = new TaskDAO(session);
+			Task task;
+			String id = formData.getValueSilently("taskId");
+			boolean isNew = id.isEmpty();
 
-            if (isNew) {
-                task = new Task();
-            } else {
-                task = dao.findById(id);
-            }
+			if (isNew) {
+				task = new Task();
+			} else {
+				task = dao.findById(id);
+			}
 
-            task.setProject(projectDAO.findById(formData.getValue("projectId")));
-            task.setTaskType(taskTypeDAO.findById(formData.getValue("taskTypeId")));
-            task.setStatus(TaskStatusType.valueOf(formData.getValueSilently("status")));
-            task.setPriority(TaskPriorityType.valueOf(formData.getValueSilently("priority")));
-            task.setStartDate(TimeUtil.convertStringToDate(formData.getValueSilently("startDate")));
-            task.setDueDate(TimeUtil.convertStringToDate(formData.getValueSilently("dueDate")));
-            task.setBody(formData.getValue("body"));
-            User assigneeUser = userDAO.findById(formData.getNumberValueSilently("assigneeUserId", 0));
-            task.setAssignee(assigneeUser.getId());
+			task.setProject(projectDAO.findById(formData.getValue("projectId")));
+			task.setTaskType(taskTypeDAO.findById(formData.getValue("taskTypeId")));
+			task.setStatus(TaskStatusType.valueOf(formData.getValueSilently("status")));
+			task.setPriority(TaskPriorityType.valueOf(formData.getValueSilently("priority")));
+			task.setStartDate(TimeUtil.convertStringToDate(formData.getValueSilently("startDate")));
+			task.setDueDate(TimeUtil.convertStringToDate(formData.getValueSilently("dueDate")));
+			task.setBody(formData.getValue("body"));
+			User assigneeUser = userDAO.findById(formData.getNumberValueSilently("assigneeUserId", 0));
+			task.setAssignee(assigneeUser.getId());
 
-            if (formData.containsField("tagIds")) {
-                String[] tagIds = formData.getListOfValuesSilently("tagIds");
-                if (tagIds.length > 0) {
-                    List<Tag> tags = new ArrayList<>();
-                    TagDAO tagDAO = new TagDAO(session);
-                    for (String tagId : tagIds) {
-                        if (!tagId.isEmpty()) {
-                            Tag tag = tagDAO.findById(UUID.fromString(tagId));
-                            if (tag != null) {
-                                tags.add(tag);
-                            }
-                        }
-                    }
-                    task.setTags(tags);
-                }
-            }
+			if (formData.containsField("tagIds")) {
+				String[] tagIds = formData.getListOfValuesSilently("tagIds");
+				if (tagIds.length > 0) {
+					List<Tag> tags = new ArrayList<>();
+					TagDAO tagDAO = new TagDAO(session);
+					for (String tagId : tagIds) {
+						if (!tagId.isEmpty()) {
+							Tag tag = tagDAO.findById(UUID.fromString(tagId));
+							if (tag != null) {
+								tags.add(tag);
+							}
+						}
+					}
+					task.setTags(tags);
+				}
+			}
 
-            String[] fileNames = formData.getListOfValuesSilently("fileid");
-            if (fileNames.length > 0) {
-                File userTmpDir = new File(Environment.tmpDir + File.separator + session.getUser().getUserID());
-                for (String fn : fileNames) {
-                    File file = new File(userTmpDir + File.separator + fn);
-                    InputStream is = new FileInputStream(file);
-                    Attachment att = new Attachment();
-                    att.setRealFileName(fn);
-                    att.setFile(IOUtils.toByteArray(is));
-                    task.getAttachments().add(att);
-                }
-            }
+			String[] fileNames = formData.getListOfValuesSilently("fileid");
+			if (fileNames.length > 0) {
+				File userTmpDir = new File(Environment.tmpDir + File.separator + session.getUser().getUserID());
+				for (String fn : fileNames) {
+					File file = new File(userTmpDir + File.separator + fn);
+					InputStream is = new FileInputStream(file);
+					Attachment att = new Attachment();
+					att.setRealFileName(fn);
+					att.setFile(IOUtils.toByteArray(is));
+					task.getAttachments().add(att);
+				}
+			}
 
-            if (isNew) {
-                IUser<Long> user = session.getUser();
-                task.addReaderEditor(user);
-                task.addReader(assigneeUser);
-                task = dao.add(task);
-            } else {
-                task = dao.update(task);
-            }
+			if (isNew) {
+				IUser<Long> user = session.getUser();
+				task.addReaderEditor(user);
+				task.addReader(assigneeUser);
+				task = dao.add(task);
+			} else {
+				task = dao.update(task);
+			}
+		} catch (SecureException e) {
+			setError(e);
+		} catch (_Exception | DatabaseException | IOException e) {
+			error(e);
+			setBadRequest();
+		}
+	}
 
-            finishSaveFormTransact(task);
-        } catch (SecureException e) {
-            setError(e);
-        } catch (_Exception | DatabaseException | IOException e) {
-            error(e);
-            setBadRequest();
-        }
-    }
+	@Override
+	public void doDELETE(_Session session, _WebFormData formData) {
+		String id = formData.getValueSilently("taskId");
+		String attachmentId = formData.getValueSilently("attachment");
 
-    @Override
-    public void doDELETE(_Session session, _WebFormData formData) {
-        String id = formData.getValueSilently("taskId");
-        String attachmentId = formData.getValueSilently("attachment");
+		if (id.isEmpty() || attachmentId.isEmpty()) {
+			return;
+		}
 
-        if (id.isEmpty() || attachmentId.isEmpty()) {
-            return;
-        }
+		TaskDAO dao = new TaskDAO(session);
+		Task task = dao.findById(id);
 
-        TaskDAO dao = new TaskDAO(session);
-        Task task = dao.findById(id);
+		AttachmentDAO attachmentDAO = new AttachmentDAO(session);
+		Attachment attachment = attachmentDAO.findById(attachmentId);
+		task.getAttachments().remove(attachment);
 
-        AttachmentDAO attachmentDAO = new AttachmentDAO(session);
-        Attachment attachment = attachmentDAO.findById(attachmentId);
-        task.getAttachments().remove(attachment);
+		try {
+			dao.update(task);
+		} catch (SecureException e) {
+			setError(e);
+		}
+	}
 
-        try {
-            dao.update(task);
-        } catch (SecureException e) {
-            setError(e);
-        }
-    }
+	private _Validation validate(_WebFormData formData, LanguageCode lang) {
+		_Validation ve = new _Validation();
 
-    private _Validation validate(_WebFormData formData, LanguageCode lang) {
-        _Validation ve = new _Validation();
+		if (formData.getValueSilently("projectId").isEmpty()) {
+			ve.addError("projectId", "required", getLocalizedWord("field_is_empty", lang));
+		}
+		if (formData.getValueSilently("taskTypeId").isEmpty()) {
+			ve.addError("taskTypeId", "required", getLocalizedWord("field_is_empty", lang));
+		}
+		if (formData.getValueSilently("body").isEmpty()) {
+			ve.addError("body", "required", getLocalizedWord("field_is_empty", lang));
+		}
+		if (formData.getValueSilently("status").isEmpty()) {
+			ve.addError("status", "required", getLocalizedWord("field_is_empty", lang));
+		}
+		if (formData.getValueSilently("priority").isEmpty()) {
+			ve.addError("priority", "required", getLocalizedWord("field_is_empty", lang));
+		}
+		String sDate = formData.getValueSilently("startDate");
+		if (sDate.isEmpty()) {
+			ve.addError("startDate", "required", getLocalizedWord("field_is_empty", lang));
+		} else if (TimeUtil.convertStringToDate(sDate) == null) {
+			ve.addError("startDate", "date", getLocalizedWord("date_format_does_not_match_to", lang) + " dd.MM.YYYY");
+		}
 
-        if (formData.getValueSilently("projectId").isEmpty()) {
-            ve.addError("projectId", "required", getLocalizedWord("field_is_empty", lang));
-        }
-        if (formData.getValueSilently("taskTypeId").isEmpty()) {
-            ve.addError("taskTypeId", "required", getLocalizedWord("field_is_empty", lang));
-        }
-        if (formData.getValueSilently("body").isEmpty()) {
-            ve.addError("body", "required", getLocalizedWord("field_is_empty", lang));
-        }
-        if (formData.getValueSilently("status").isEmpty()) {
-            ve.addError("status", "required", getLocalizedWord("field_is_empty", lang));
-        }
-        if (formData.getValueSilently("priority").isEmpty()) {
-            ve.addError("priority", "required", getLocalizedWord("field_is_empty", lang));
-        }
-        String sDate = formData.getValueSilently("startDate");
-        if (sDate.isEmpty()) {
-            ve.addError("startDate", "required", getLocalizedWord("field_is_empty", lang));
-        } else if (TimeUtil.convertStringToDate(sDate) == null) {
-            ve.addError("startDate", "date", getLocalizedWord("date_format_does_not_match_to", lang) + " dd.MM.YYYY");
-        }
+		String dDate = formData.getValueSilently("startDate");
+		if (dDate.isEmpty()) {
+			ve.addError("dueDate", "required", getLocalizedWord("field_is_empty", lang));
+		} else if (TimeUtil.convertStringToDate(dDate) == null) {
+			ve.addError("dueDate", "date", getLocalizedWord("date_format_does_not_match_to", lang) + " dd.MM.YYYY");
+		}
 
-        String dDate = formData.getValueSilently("startDate");
-        if (dDate.isEmpty()) {
-            ve.addError("dueDate", "required", getLocalizedWord("field_is_empty", lang));
-        } else if (TimeUtil.convertStringToDate(dDate) == null) {
-            ve.addError("dueDate", "date", getLocalizedWord("date_format_does_not_match_to", lang) + " dd.MM.YYYY");
-        }
+		if (formData.getNumberValueSilently("assigneeUserId", 0) == 0) {
+			ve.addError("assigneeUserId", "required", getLocalizedWord("field_is_empty", lang));
+		}
 
-        if (formData.getNumberValueSilently("assigneeUserId", 0) == 0) {
-            ve.addError("assigneeUserId", "required", getLocalizedWord("field_is_empty", lang));
-        }
-
-        return ve;
-    }
+		return ve;
+	}
 }
