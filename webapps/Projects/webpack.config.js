ff58c9f20d318d00e3d05bf2f79ab2e92dc33d5f
medin84 <kalihanovm@gmail.com>
2016-07-15 12:00:36 +0600

    config

--- webapps/Projects/webpack.config.js
+++ webapps/Projects/webpack.config.js
@@ -1,98 +1,48 @@
-'use strict';
+const path               = require('path');
+// Webpack and its plugins
+const webpack            = require('webpack');
+const CommonsChunkPlugin = require('webpack/lib/optimize/CommonsChunkPlugin');
+// const CopyWebpackPlugin  = require('copy-webpack-plugin');
+const DefinePlugin       = require('webpack/lib/DefinePlugin');
+const ProvidePlugin      = require('webpack/lib/ProvidePlugin');
 
-// webpack -w
-// set NODE_ENV=production & webpack -p --optimize-minimize --optimize-dedupe
+const ENV  = process.env.NODE_ENV = 'development';
 
-const path = require("path");
-const webpack = require('webpack');
-const HtmlWebpackPlugin = require('html-webpack-plugin');
-const CompressionPlugin = require('compression-webpack-plugin');
-
-const basePlugins = [
-    new webpack.DefinePlugin({
-        __DEV__: process.env.NODE_ENV !== 'production',
-        __PRODUCTION__: process.env.NODE_ENV === 'production',
-        'process.env.NODE_ENV': JSON.stringify(process.env.NODE_ENV)
-    }),
-    new webpack.optimize.CommonsChunkPlugin({
-        name: ['app', 'vendor']
-    }),
-    /*new HtmlWebpackPlugin({
-        template: './src/index.html',
-        inject: 'body',
-        minify: false
-    }),*/
-    new webpack.NoErrorsPlugin()
-];
-
-const devPlugins = [];
-
-const prodPlugins = [
-    new webpack.optimize.DedupePlugin(),
-    new webpack.optimize.UglifyJsPlugin({
-        beautify: false,
-        mangle: true,
-        comments: false,
-        sourceMap: false,
-        compress: {
-            warnings: false
-        },
-        output: {
-            comments: false
-        }
-    }),
-    // new CompressionPlugin({
-    //     asset: 'vendor.js.gz',
-    //     algorithm: 'gzip',
-    //     regExp: /\.js$|\.html|\.css|.map$/,
-    //     threshold: 10240,
-    //     minRatio: 0.8
-    // })
-];
-
-const plugins = basePlugins
-    .concat(process.env.NODE_ENV === 'production' ? prodPlugins : [])
-    .concat(process.env.NODE_ENV !== 'production' ? devPlugins : []);
+const metadata = {
+  baseUrl: '/',
+  ENV    : ENV,
+};
 
 module.exports = {
-
-    debug: true,
-
-    devtool: process.env.NODE_ENV === 'production' ? false : 'inline-source-map',
-
-    entry: {
-        app: './app/main.browser.ts',
-        vendor: './app/vendors.ts'
-    },
-
-    output: {
-        path: path.resolve(__dirname, 'js'),
-        filename: '[name].js',
-        publicPath: '/js/'
-    },
-
-    resolve: {
-        extensions: ['', '.ts', '.js']
-    },
-
-    plugins: plugins,
-
-    module: {
-        loaders: [{
-                test: /\.ts$/,
-                loader: 'ts-loader',
-                exclude: /node_modules/
-            }, {
-                test: /\.html$/,
-                loader: 'raw'
-            },
-            /*{ test: /\.css$/, loader: 'style-loader!css-loader?sourceMap' },
-            { test: /\.svg/, loader: 'url' },
-            { test: /\.eot/, loader: 'url' },
-            { test: /\.woff/, loader: 'url' },
-            { test: /\.woff2/, loader: 'url' },
-            { test: /\.ttf/, loader: 'url' },*/
-        ],
-        noParse: [/zone\.js\/dist\/.+/]
-    }
-}
+  debug: true,
+  devtool: 'source-map',
+  entry: {
+    'app'  : './app/main.browser.ts',
+    'vendor': './app/vendors.ts'
+  },
+  module: {
+    loaders: [
+      {test: /\.css$/,   loader: 'raw', exclude: /node_modules/},
+      {test: /\.css$/,   loader: 'style!css?-minimize', exclude: /src/},
+      {test: /\.html$/,  loader: 'raw'},
+      {test: /\.ts$/,    loader: 'ts', query: {compilerOptions: {noEmit: false}}},
+      {test: /\.woff$/,  loader: 'url?limit=10000&mimetype=application/font-woff'},
+      {test: /\.woff2$/, loader: 'url?limit=10000&mimetype=application/font-woff'},
+      {test: /\.ttf$/,   loader: 'url?limit=10000&mimetype=application/octet-stream'},
+      {test: /\.svg$/,   loader: 'url?limit=10000&mimetype=image/svg+xml'},
+      {test: /\.eot$/,   loader: 'file'}
+    ]
+  },
+  output: {
+      path: path.resolve(__dirname, 'js'),
+      filename: '[name].js',
+      publicPath: '/js/'
+  },
+  plugins: [
+    new CommonsChunkPlugin({name: 'vendor', filename: 'vendor.js', minChunks: Infinity}),
+    new DefinePlugin({'webpack': {'ENV': JSON.stringify(metadata.ENV)}})
+  ],
+  resolve: {
+    extensions: ['', '.ts', '.js']
+  }
+};
---
+++ webapps/Projects/webpack.prod.config.js
@@ -0,0 +1,82 @@
+const path = require('path');
+// Webpack and its plugins
+const webpack = require('webpack');
+const CommonsChunkPlugin = require('webpack/lib/optimize/CommonsChunkPlugin');
+const CompressionPlugin = require('compression-webpack-plugin');
+// const CopyWebpackPlugin = require('copy-webpack-plugin');
+const DedupePlugin = require('webpack/lib/optimize/DedupePlugin');
+const DefinePlugin = require('webpack/lib/DefinePlugin');
+const OccurenceOrderPlugin = require('webpack/lib/optimize/OccurenceOrderPlugin');
+const ProvidePlugin = require('webpack/lib/ProvidePlugin');
+const UglifyJsPlugin = require('webpack/lib/optimize/UglifyJsPlugin');
+
+const ENV = process.env.NODE_ENV = 'production';
+const metadata = {
+    baseUrl: '/',
+    ENV: ENV
+};
+
+module.exports = {
+    debug: false,
+    devtool: 'source-map',
+    entry: {
+        'app': './app/main.browser.ts',
+        'vendor': './app/vendors.ts'
+    },
+    metadata: metadata,
+    module: {
+        loaders: [{
+            test: /\.css$/,
+            loader: 'to-string!css',
+            exclude: /node_modules/
+        }, {
+            test: /\.css$/,
+            loader: 'style!css',
+            exclude: /src/
+        }, {
+            test: /\.html$/,
+            loader: 'html?caseSensitive=true'
+        }, {
+            test: /\.ts$/,
+            loader: 'ts',
+            query: {
+                compilerOptions: {
+                    noEmit: false
+                }
+            }
+        }, {
+            test: /\.woff$/,
+            loader: "url?limit=10000&minetype=application/font-woff"
+        }, {
+            test: /\.woff2$/,
+            loader: "url?limit=10000&minetype=application/font-woff"
+        }, {
+            test: /\.ttf$/,
+            loader: "url?limit=10000&minetype=application/octet-stream"
+        }, {
+            test: /\.svg$/,
+            loader: "url?limit=10000&minetype=image/svg+xml"
+        }, {
+            test: /\.eot$/,
+            loader: "file"
+        }],
+        noParse: [/zone\.js\/dist\/.+/]
+    },
+    output: {
+        path: path.resolve(__dirname, 'js'),
+        filename: '[name].js',
+        publicPath: '/js/'
+    },
+    plugins: [
+        new CommonsChunkPlugin({name: 'vendor', filename: 'vendor.js', minChunks: Infinity}),
+        // new CompressionPlugin({regExp: /\.css$|\.html$|\.js$|\.map$/, threshold: 1500}),
+        // new CopyWebpackPlugin([{from: './src/index.html', to: 'index.html'}]),
+        new DedupePlugin(),
+        new DefinePlugin({'webpack': {'ENV': JSON.stringify(metadata.ENV)}}),
+        new OccurenceOrderPlugin(true),
+        new UglifyJsPlugin({compress: {screw_ie8: true}, mangle: {screw_ie8: true}})
+    ],
+    resolve: {
+        extensions: ['', '.ts', '.js']
+    }
+};
