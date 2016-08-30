857415e6e1c58ed67682f375f57b9dcb73498e25
k-zon_000 <k-zon_000@KayraNote>
2016-08-30 22:49:35 +0300

    refactor

--- rule/Projects/Resources/scripts/projects/page/view/ProjectView.java
+++ rule/Projects/Resources/scripts/projects/page/view/ProjectView.java
@@ -2,36 +2,36 @@ package projects.page.view;
 
 import com.exponentus.dataengine.jpa.ViewPage;
 import com.exponentus.exception.SecureException;
-import com.exponentus.scripting._POJOListWrapper;
 import com.exponentus.scripting._Session;
 import com.exponentus.scripting._WebFormData;
 import com.exponentus.scripting.event._DoPage;
+
 import projects.dao.ProjectDAO;
 import projects.model.Project;
 
 public class ProjectView extends _DoPage {
 
-    @Override
-    public void doGET(_Session session, _WebFormData formData) {
-        ProjectDAO projectDAO = new ProjectDAO(session);
-        String keyWord = formData.getAnyValueSilently("keyWord");
-        int pageSize = 200; // session.pageSize;
-        int pageNum = formData.getNumberValueSilently("page", 0);
-        ViewPage<Project> vp = projectDAO.findProjects(keyWord, pageNum, pageSize);
-        addContent(new _POJOListWrapper(vp.getResult(), vp.getMaxPage(), vp.getCount(), vp.getPageNum(), session));
-    }
+	@Override
+	public void doGET(_Session session, _WebFormData formData) {
+		ProjectDAO projectDAO = new ProjectDAO(session);
+		String keyWord = formData.getAnyValueSilently("keyWord");
+		int pageSize = 200; // session.pageSize;
+		int pageNum = formData.getNumberValueSilently("page", 0);
+		ViewPage<Project> vp = projectDAO.findProjects(keyWord, pageNum, pageSize);
+		addContent(vp.getResult(), vp.getMaxPage(), vp.getCount(), vp.getPageNum());
+	}
 
-    @Override
-    public void doDELETE(_Session session, _WebFormData formData) {
-        ProjectDAO projectDAO = new ProjectDAO(session);
-        for (String id : formData.getListOfValuesSilently("projectIds")) {
-            Project m = projectDAO.findById(id);
-            try {
-                m.setAttachments(null); // if no on delete cascade
-                projectDAO.delete(m);
-            } catch (SecureException e) {
-                setError(e);
-            }
-        }
-    }
+	@Override
+	public void doDELETE(_Session session, _WebFormData formData) {
+		ProjectDAO projectDAO = new ProjectDAO(session);
+		for (String id : formData.getListOfValuesSilently("projectIds")) {
+			Project m = projectDAO.findById(id);
+			try {
+				m.setAttachments(null); // if no on delete cascade
+				projectDAO.delete(m);
+			} catch (SecureException e) {
+				setError(e);
+			}
+		}
+	}
 }
--- rule/Projects/Resources/scripts/projects/page/view/TaskView.java
+++ rule/Projects/Resources/scripts/projects/page/view/TaskView.java
@@ -1,12 +1,16 @@
 package projects.page.view;
 
+import java.util.ArrayList;
+import java.util.List;
+import java.util.UUID;
+
 import com.exponentus.dataengine.jpa.ViewPage;
 import com.exponentus.exception.SecureException;
 import com.exponentus.scripting._Exception;
-import com.exponentus.scripting._POJOListWrapper;
 import com.exponentus.scripting._Session;
 import com.exponentus.scripting._WebFormData;
 import com.exponentus.scripting.event._DoPage;
+
 import projects.dao.TaskDAO;
 import projects.dao.filter.TaskFilter;
 import projects.model.Request;
@@ -15,120 +19,116 @@ import projects.model.constants.TaskPriorityType;
 import projects.model.constants.TaskStatusType;
 import reference.model.Tag;
 
-import java.util.ArrayList;
-import java.util.List;
-import java.util.UUID;
-
 public class TaskView extends _DoPage {
 
-    @Override
-    public void doGET(_Session session, _WebFormData formData) {
-        if (formData.containsField("stream")) {
-            responseTaskStream(session, formData);
-        } else {
-            responseTaskList(session, formData);
-        }
-    }
+	@Override
+	public void doGET(_Session session, _WebFormData formData) {
+		if (formData.containsField("stream")) {
+			responseTaskStream(session, formData);
+		} else {
+			responseTaskList(session, formData);
+		}
+	}
 
-    @Override
-    public void doDELETE(_Session session, _WebFormData formData) {
-        TaskDAO taskDAO = new TaskDAO(session);
-        for (String id : formData.getListOfValuesSilently("taskIds")) {
-            Task m = taskDAO.findById(id);
-            try {
-                m.setAttachments(null); // if no on delete cascade
-                taskDAO.delete(m);
-            } catch (SecureException e) {
-                setError(e);
-            }
-        }
-    }
+	@Override
+	public void doDELETE(_Session session, _WebFormData formData) {
+		TaskDAO taskDAO = new TaskDAO(session);
+		for (String id : formData.getListOfValuesSilently("taskIds")) {
+			Task m = taskDAO.findById(id);
+			try {
+				m.setAttachments(null); // if no on delete cascade
+				taskDAO.delete(m);
+			} catch (SecureException e) {
+				setError(e);
+			}
+		}
+	}
 
-    private void responseTaskList(_Session session, _WebFormData formData) {
-        TaskDAO taskDAO = new TaskDAO(session);
-        TaskFilter taskFilter = createTaskFilter(session, formData);
-        int pageSize = session.pageSize;
-        int pageNum = formData.getNumberValueSilently("page", 0);
-        ViewPage<Task> vp = taskDAO.findAllByTaskFilter(taskFilter, pageNum, pageSize);
-        addContent(new _POJOListWrapper(vp.getResult(), vp.getMaxPage(), vp.getCount(), vp.getPageNum(), session));
-    }
+	private void responseTaskList(_Session session, _WebFormData formData) {
+		TaskDAO taskDAO = new TaskDAO(session);
+		TaskFilter taskFilter = createTaskFilter(session, formData);
+		int pageSize = session.pageSize;
+		int pageNum = formData.getNumberValueSilently("page", 0);
+		ViewPage<Task> vp = taskDAO.findAllByTaskFilter(taskFilter, pageNum, pageSize);
+		addContent(vp.getResult(), vp.getMaxPage(), vp.getCount(), vp.getPageNum());
+	}
 
-    private void responseTaskStream(_Session session, _WebFormData formData) {
-        try {
-            String taskId = formData.getValue("taskId");
-            TaskDAO taskDAO = new TaskDAO(session);
-            Task task = taskDAO.findById(taskId);
+	private void responseTaskStream(_Session session, _WebFormData formData) {
+		try {
+			String taskId = formData.getValue("taskId");
+			TaskDAO taskDAO = new TaskDAO(session);
+			Task task = taskDAO.findById(taskId);
 
-            if (task == null) {
-                addContent("message", "task not found");
-                return;
-            }
+			if (task == null) {
+				addContent("message", "task not found");
+				return;
+			}
 
-            List<Task> subTasks = task.getSubtasks();
-            List<Request> requests = task.getRequests();
+			List<Task> subTasks = task.getSubtasks();
+			List<Request> requests = task.getRequests();
 
-            if (subTasks.size() > 0) {
-                addContent(subTasks);
-            }
-            if (requests.size() > 0) {
-                addContent(requests);
-            }
-        } catch (_Exception e) {
-            setError(e);
-            e.printStackTrace();
-        }
-    }
+			if (subTasks.size() > 0) {
+				addContent(subTasks);
+			}
+			if (requests.size() > 0) {
+				addContent(requests);
+			}
+		} catch (_Exception e) {
+			setError(e);
+			e.printStackTrace();
+		}
+	}
 
-    private static TaskFilter createTaskFilter(_Session session, _WebFormData formData) {
-        TaskFilter filter = new TaskFilter();
+	private static TaskFilter createTaskFilter(_Session session, _WebFormData formData) {
+		TaskFilter filter = new TaskFilter();
 
-        filter.setProject(formData.getValueSilently("projectId"));
-        filter.setParentTask(formData.getValueSilently("parentTaskId"));
-        filter.setTaskType(formData.getValueSilently("taskTypeId"));
-        filter.setSearch(formData.getValueSilently("keyWord").toLowerCase());
-        filter.setStartDate(formData.getDateSilently("startDate"));
-        filter.setDueDate(formData.getDateSilently("dueDate"));
+		filter.setProject(formData.getValueSilently("projectId"));
+		filter.setParentTask(formData.getValueSilently("parentTaskId"));
+		filter.setTaskType(formData.getValueSilently("taskTypeId"));
+		filter.setSearch(formData.getValueSilently("keyWord").toLowerCase());
+		filter.setStartDate(formData.getDateSilently("startDate"));
+		filter.setDueDate(formData.getDateSilently("dueDate"));
 
-        String parentOnly = formData.getValueSilently("parentOnly");
-        if (!filter.hasSearch() && !parentOnly.equals("false")) {
-            filter.setParentOnly(true);
-        } else {
-            filter.setParentOnly(false);
-        }
+		String parentOnly = formData.getValueSilently("parentOnly");
+		if (!filter.hasSearch() && !parentOnly.equals("false")) {
+			filter.setParentOnly(true);
+		} else {
+			filter.setParentOnly(false);
+		}
 
-        String taskStatus = formData.getValueSilently("taskStatus");
-        if (!taskStatus.isEmpty()) {
-            filter.setStatus(TaskStatusType.valueOf(taskStatus));
-        }
+		String taskStatus = formData.getValueSilently("taskStatus");
+		if (!taskStatus.isEmpty()) {
+			filter.setStatus(TaskStatusType.valueOf(taskStatus));
+		}
 
-        String taskPriority = formData.getValueSilently("taskPriority");
-        if (!taskPriority.isEmpty()) {
-            filter.setPriority(TaskPriorityType.valueOf(taskPriority));
-        }
+		String taskPriority = formData.getValueSilently("taskPriority");
+		if (!taskPriority.isEmpty()) {
+			filter.setPriority(TaskPriorityType.valueOf(taskPriority));
+		}
 
-        long assigneeUserId = (long) formData.getNumberDoubleValueSilently("assigneeUserId", 0);
-        if (assigneeUserId > 0) {
-            filter.setAssigneeUserId(assigneeUserId);
-        }
+		long assigneeUserId = (long) formData.getNumberDoubleValueSilently("assigneeUserId", 0);
+		if (assigneeUserId > 0) {
+			filter.setAssigneeUserId(assigneeUserId);
+		}
 
-        String tasksFor = formData.getValueSilently("for");
-        if ("inbox".equals(tasksFor)) {
-            filter.setAssigneeUserId(session.getUser().getId());
-        } else if ("my".equals(tasksFor)) {
-            filter.setAuthorId(session.getUser().getId());
-        }
+		String tasksFor = formData.getValueSilently("for");
+		if ("inbox".equals(tasksFor)) {
+			filter.setAssigneeUserId(session.getUser().getId());
+		} else if ("my".equals(tasksFor)) {
+			filter.setAuthorId(session.getUser().getId());
+		}
 
-        if (formData.containsField("tagIds")) {
-            List<Tag> tags = new ArrayList<>();
-            String[] tagIds = formData.getListOfValuesSilently("tagIds");
-            for (String tid : tagIds) {
-                Tag tag = new Tag();
-                tag.setId(UUID.fromString(tid));
-                tags.add(tag);
-            }
-            filter.setTags(tags);
-        }
+		if (formData.containsField("tagIds")) {
+			List<Tag> tags = new ArrayList<>();
+			String[] tagIds = formData.getListOfValuesSilently("tagIds");
+			for (String tid : tagIds) {
+				Tag tag = new Tag();
+				tag.setId(UUID.fromString(tid));
+				tags.add(tag);
+			}
+			filter.setTags(tags);
+		}
 
-        return filter;
-    }
+		return filter;
+	}
 }
