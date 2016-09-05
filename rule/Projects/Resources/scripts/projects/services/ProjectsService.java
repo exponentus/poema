b474e055ca7d5668e9b4e85b327ce959c2e802c5
k-zon_000 <k-zon_000@KayraNote>
2016-09-05 13:10:27 +0300

    #

--- rule/Projects/Resources/scripts/projects/services/ProjectsService.java
+++ rule/Projects/Resources/scripts/projects/services/ProjectsService.java
@@ -1,7 +1,10 @@
 package projects.services;
 
+import javax.servlet.http.HttpServletResponse;
 import javax.ws.rs.GET;
+import javax.ws.rs.POST;
 import javax.ws.rs.Path;
+import javax.ws.rs.PathParam;
 import javax.ws.rs.Produces;
 import javax.ws.rs.core.Context;
 import javax.ws.rs.core.MediaType;
@@ -10,6 +13,7 @@ import javax.ws.rs.core.Response;
 import javax.ws.rs.core.UriInfo;
 
 import com.exponentus.dataengine.jpa.ViewPage;
+import com.exponentus.exception.SecureException;
 import com.exponentus.rest.RestProvider;
 import com.exponentus.scripting._Session;
 import com.exponentus.scripting._WebFormData;
@@ -23,19 +27,47 @@ import projects.page.view.TaskView;
 public class ProjectsService extends RestProvider {
 
 	@GET
-	@Path("/get-tasks")
+	@Path("/tasks")
 	@Produces(MediaType.APPLICATION_JSON)
-	public Response get(@Context UriInfo uriInfo) {
+	public Response getTasks(@Context UriInfo uriInfo) {
+		_Session ses = getSession();
+		TaskDAO taskDAO = new TaskDAO(ses);
 		MultivaluedMap<String, String> queryParams = uriInfo.getQueryParameters();
 		String referrer = request.getHeader("referer");
 		_WebFormData formData = new _WebFormData(queryParams, referrer);
-		_Session ses = getSession();
-		TaskDAO taskDAO = new TaskDAO(ses);
 		TaskFilter taskFilter = TaskView.createTaskFilter(ses, formData);
 		int pageSize = ses.pageSize;
 		int pageNum = formData.getNumberValueSilently("page", 0);
 		ViewPage<Task> vp = taskDAO.findAllByTaskFilter(taskFilter, pageNum, pageSize);
 		return Response.ok(vp).build();
+
+	}
+
+	@GET
+	@Path("/tasks/{id}")
+	@Produces(MediaType.APPLICATION_JSON)
+	public Response getTasks(@PathParam("id") String id) {
+		_Session ses = getSession();
+		TaskDAO taskDAO = new TaskDAO(ses);
+		Task entity = taskDAO.findById(id);
+		return Response.ok(new ViewPage<Task>(entity)).build();
+
+	}
+
+	@POST
+	@Path("/task")
+	@Produces(MediaType.APPLICATION_JSON)
+	public Response postTasks(Task task) {
+		_Session ses = getSession();
+		TaskDAO taskDAO = new TaskDAO(ses);
+		Task entity = null;
+		try {
+			entity = taskDAO.update(task);
+		} catch (SecureException e) {
+			return Response.status(HttpServletResponse.SC_BAD_REQUEST).build();
+		}
+		return Response.ok(new ViewPage<Task>(entity)).build();
+
 	}
 
 }
