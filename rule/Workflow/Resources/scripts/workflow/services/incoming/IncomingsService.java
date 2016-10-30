e40968562d3f081f64b4a81b1c22fdc42e7e2735
medin84 <kalihanovm@gmail.com>
2016-10-30 21:55:58 +0600

    pagination

--- rule/Workflow/Resources/scripts/workflow/services/incoming/IncomingsService.java
+++ rule/Workflow/Resources/scripts/workflow/services/incoming/IncomingsService.java
@@ -7,16 +7,12 @@ import com.exponentus.rest.ServiceMethod;
 import com.exponentus.rest.outgoingpojo.Outcome;
 import com.exponentus.scripting._ColumnOptions;
 import com.exponentus.scripting._Session;
-import com.exponentus.scripting._WebFormData;
 import com.exponentus.scripting.actions._Action;
 import com.exponentus.scripting.actions._ActionBar;
 import com.exponentus.scripting.actions._ActionType;
 import workflow.dao.IncomingDAO;
 
-import javax.ws.rs.GET;
-import javax.ws.rs.HttpMethod;
-import javax.ws.rs.Path;
-import javax.ws.rs.Produces;
+import javax.ws.rs.*;
 import javax.ws.rs.core.Context;
 import javax.ws.rs.core.MediaType;
 import javax.ws.rs.core.Response;
@@ -27,11 +23,9 @@ public class IncomingsService extends RestProvider {
 
     @GET
     @Produces(MediaType.APPLICATION_JSON)
-    public Response get(@Context UriInfo uriInfo) {
+    public Response get(@Context UriInfo uriInfo, @DefaultValue("1") @QueryParam("page") int pageNum) {
         _Session session = getSession();
-        _WebFormData formData = new _WebFormData(uriInfo.getQueryParameters(), httpRequest.getHeader("referer"));
         int pageSize = session.pageSize;
-        int pageNum = formData.getNumberValueSilently("page", 0);
 
         IncomingDAO incomingDAO = new IncomingDAO(session);
         ViewPage vp = incomingDAO.findViewPage(pageNum, pageSize); // formData.getSortParams(_SortParams.desc("regDate")),
