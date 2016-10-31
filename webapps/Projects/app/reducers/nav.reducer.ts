054418a6354e9026a947b4712e52da96b0ca0118
medin84 <kalihanovm@gmail.com>
2016-10-31 23:30:09 +0600

    commit

---
+++ webapps/Projects/app/reducers/nav.reducer.ts
@@ -0,0 +1,22 @@
+import { Tag } from '../models';
+
+import { NavActions } from '../actions/nav.actions';
+
+export interface INavState {
+    reload: any
+};
+
+const initialState: INavState = {
+    reload: {}
+};
+
+export const navReducer = (state = initialState, {type, payload}): INavState => {
+    switch (type) {
+        case NavActions.RELOAD_NAV:
+            return Object.assign({}, state, {
+                reload: payload
+            });
+        default:
+            return state;
+    }
+};
