4fbd9763c0fb38f20d83fd09fea599b7464f5208
kayra <justaidajam@gmail.com>
2016-11-10 21:41:38 +0200

    #

--- rule/Workflow/Resources/scripts/workflow/model/Assignment.java
+++ rule/Workflow/Resources/scripts/workflow/model/Assignment.java
@@ -3,13 +3,11 @@ package workflow.model;
 import java.util.List;
 import java.util.UUID;
 
-import javax.persistence.CascadeType;
 import javax.persistence.Column;
+import javax.persistence.Embedded;
 import javax.persistence.Entity;
-import javax.persistence.JoinTable;
 import javax.persistence.ManyToOne;
 import javax.persistence.NamedQuery;
-import javax.persistence.OneToOne;
 import javax.persistence.Table;
 import javax.validation.constraints.NotNull;
 
@@ -23,87 +21,86 @@ import workflow.model.embedded.Control;
 @Table(name = "assignments")
 @NamedQuery(name = "Assignment.findAll", query = "SELECT m FROM Assignment AS m ORDER BY m.regDate")
 public class Assignment extends SecureAppEntity<UUID> {
-
+	
 	@Column(name = "applied_author", nullable = false, updatable = true)
 	protected Long appliedAuthor;
-
+	
 	@FTSearchable
 	@Column(length = 140)
 	private String title;
-
+	
 	@NotNull
 	@ManyToOne
 	private Incoming incoming;
-
+	
 	@NotNull
 	@ManyToOne
 	private Assignment parent;
-
+	
 	@FTSearchable
 	@Column(columnDefinition = "TEXT")
 	private String body;
-
+	
 	@JsonProperty("observerUserIds")
 	private List<Long> observers;
-
-	@OneToOne(cascade = CascadeType.ALL)
-	@JoinTable(name = "assignment_control")
+	
+	@Embedded
 	private Control control;
-
+	
 	public Long getAppliedAuthor() {
 		return appliedAuthor;
 	}
-
+	
 	public void setAppliedAuthor(long appliedAuthor) {
 		this.appliedAuthor = appliedAuthor;
 	}
-
+	
 	public String getTitle() {
 		return title;
 	}
-
+	
 	public void setTitle(String title) {
 		this.title = title;
 	}
-
+	
 	public Incoming getIncoming() {
 		return incoming;
 	}
-
+	
 	public void setIncoming(Incoming incoming) {
 		this.incoming = incoming;
 	}
-
+	
 	public Assignment getParent() {
 		return parent;
 	}
-
+	
 	public void setParent(Assignment parent) {
 		this.parent = parent;
 	}
-
+	
 	public String getBody() {
 		return body;
 	}
-
+	
 	public void setBody(String body) {
 		this.body = body;
 	}
-
+	
 	public List<Long> getObservers() {
 		return observers;
 	}
-
+	
 	public void setObservers(List<Long> observers) {
 		this.observers = observers;
 	}
-
+	
 	public Control getControl() {
 		return control;
 	}
-
+	
 	public void setControl(Control control) {
 		this.control = control;
 	}
-
+	
 }
--- rule/Workflow/Resources/scripts/workflow/model/embedded/Control.java
+++ rule/Workflow/Resources/scripts/workflow/model/embedded/Control.java
@@ -6,11 +6,10 @@ import java.util.List;
 import javax.persistence.CascadeType;
 import javax.persistence.Column;
 import javax.persistence.Convert;
-import javax.persistence.Entity;
+import javax.persistence.Embeddable;
 import javax.persistence.JoinTable;
 import javax.persistence.ManyToOne;
 import javax.persistence.OneToMany;
-import javax.persistence.Table;
 import javax.persistence.Temporal;
 import javax.persistence.TemporalType;
 import javax.validation.constraints.NotNull;
@@ -21,67 +20,66 @@ import reference.model.ControlType;
 import workflow.model.constants.ControlStatusType;
 import workflow.model.util.ControlStatusTypeConverter;
 
-@Entity
-@Table(name = "controls")
+@Embeddable
 public class Control extends SimpleAppEntity {
-	
+
 	@NotNull
 	@ManyToOne(optional = false)
 	private ControlType controlType;
-	
+
 	@Temporal(TemporalType.TIMESTAMP)
 	@Column(name = "start_date")
 	private Date startDate;
-	
+
 	@Temporal(TemporalType.TIMESTAMP)
 	@Column(name = "due_date")
 	private Date dueDate;
-	
+
 	@Convert(converter = ControlStatusTypeConverter.class)
 	private ControlStatusType status = ControlStatusType.UNKNOWN;
-	
+
 	@OneToMany(cascade = CascadeType.ALL)
 	@JoinTable(name = "control_assigneeentries")
 	private List<AssigneeEntry> assigneeEntries;
-	
+
 	public ControlType getControlType() {
 		return controlType;
 	}
-	
+
 	public void setControlType(ControlType controlType) {
 		this.controlType = controlType;
 	}
-	
+
 	public Date getStartDate() {
 		return startDate;
 	}
-	
+
 	public void setStartDate(Date startDate) {
 		this.startDate = startDate;
 	}
-	
+
 	public Date getDueDate() {
 		return dueDate;
 	}
-	
+
 	public void setDueDate(Date dueDate) {
 		this.dueDate = dueDate;
 	}
-	
+
 	public ControlStatusType getStatus() {
 		return status;
 	}
-	
+
 	public void setStatus(ControlStatusType status) {
 		this.status = status;
 	}
-	
+
 	public List<AssigneeEntry> getAssigneeEntries() {
 		return assigneeEntries;
 	}
-	
+
 	public void setAssigneeEntries(List<AssigneeEntry> assigneeEntries) {
 		this.assigneeEntries = assigneeEntries;
 	}
-	
+
 }
