b3c410b7c18879b19a7da7a8ea262afb9f195163
kayra <justaidajam@gmail.com>
2016-11-11 12:01:59 +0200

    #

--- rule/HelpDesk/Resources/scripts/helpdesk/model/constants/DemandStatusType.java
+++ rule/HelpDesk/Resources/scripts/helpdesk/model/constants/DemandStatusType.java
@@ -4,18 +4,18 @@ package helpdesk.model.constants;
  * @author Kayra created 6-11-2016
  */
 public enum DemandStatusType {
-	UNKNOWN(0);
-
+	UNKNOWN(0), DRAFT(566), PROCESSING(567), COMPLETED(568), CANCELLED(569), OPEN(570);
+	
 	private int code;
-
+	
 	DemandStatusType(int code) {
 		this.code = code;
 	}
-
+	
 	public int getCode() {
 		return code;
 	}
-
+	
 	public static DemandStatusType getType(int code) {
 		for (DemandStatusType type : values()) {
 			if (type.code == code) {
