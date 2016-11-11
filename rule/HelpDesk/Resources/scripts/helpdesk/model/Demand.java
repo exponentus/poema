55ccd878857a51cdc0739561905d8622a5f01e38
kayra <justaidajam@gmail.com>
2016-11-11 11:46:59 +0200

    #

--- rule/HelpDesk/Resources/scripts/helpdesk/model/Demand.java
+++ rule/HelpDesk/Resources/scripts/helpdesk/model/Demand.java
@@ -15,6 +15,7 @@ import javax.persistence.Index;
 import javax.persistence.JoinColumn;
 import javax.persistence.JoinTable;
 import javax.persistence.ManyToMany;
+import javax.persistence.ManyToOne;
 import javax.persistence.NamedAttributeNode;
 import javax.persistence.NamedEntityGraph;
 import javax.persistence.NamedEntityGraphs;
@@ -36,6 +37,7 @@ import com.fasterxml.jackson.annotation.JsonRootName;
 
 import helpdesk.model.constants.DemandStatusType;
 import projects.model.Project;
+import reference.model.DemandType;
 import reference.model.Tag;
 import staff.model.Organization;
 
@@ -52,37 +54,41 @@ import staff.model.Organization;
 				@NamedSubgraph(name = "attachments", attributeNodes = { @NamedAttributeNode("id"),
 						@NamedAttributeNode("realFileName"), @NamedAttributeNode("size") }) }) })
 public class Demand extends SecureAppEntity<UUID> {
-	
-	public final static String SHORT_GRAPH = "Demand.SHORT_GRAPH";
 
-	private Project project;
+	public final static String SHORT_GRAPH = "Demand.SHORT_GRAPH";
 	
+	private Project project;
+
 	@FTSearchable(ignoreLang = true)
 	@Column(name = "reg_number", length = 140)
 	private String regNumber;
-	
+
 	@Enumerated(EnumType.STRING)
 	@Column(length = 10)
 	private DemandStatusType status = DemandStatusType.UNKNOWN;
-	
+
 	@Temporal(TemporalType.TIMESTAMP)
 	@Column(name = "status_date")
 	private Date statusDate;
-	
+
 	@FTSearchable
 	@Column(length = 140)
 	private String title;
 	
+	@ManyToOne(optional = false)
+	@JoinColumn(name = "demand_type")
+	private DemandType demandType;
+
 	private Organization customer;
-	
+
 	@FTSearchable
 	@Column(columnDefinition = "TEXT")
 	private String body;
-	
+
 	@ManyToMany(fetch = FetchType.EAGER)
 	@JoinTable(name = "demand_tags")
 	private List<Tag> tags;
-	
+
 	@OneToMany(fetch = FetchType.EAGER, cascade = CascadeType.ALL)
 	@JoinTable(name = "demand_attachments", joinColumns = { @JoinColumn(name = "demand_id") }, inverseJoinColumns = {
 			@JoinColumn(name = "attachment_id") }, indexes = {
@@ -90,55 +96,63 @@ public class Demand extends SecureAppEntity<UUID> {
 							"demand_id", "attachment_id" }))
 	@CascadeOnDelete
 	private List<Attachment> attachments = new ArrayList<>();
-	
+
 	public Project getProject() {
 		return project;
 	}
-
+	
 	public void setProject(Project project) {
 		this.project = project;
 	}
-
+	
 	public DemandStatusType getStatus() {
 		return status;
 	}
-
+	
 	public void setStatus(DemandStatusType status) {
 		this.status = status;
 		statusDate = new Date();
 	}
-
+	
 	public String getTitle() {
 		return title;
 	}
-
+	
 	public void setTitle(String title) {
 		this.title = title;
 	}
-
+	
+	public DemandType getDemandType() {
+		return demandType;
+	}
+	
+	public void setDemandType(DemandType demandType) {
+		this.demandType = demandType;
+	}
+	
 	public Organization getCustomer() {
 		return customer;
 	}
-
+	
 	public void setCustomer(Organization customer) {
 		this.customer = customer;
 	}
-
+	
 	public boolean isHasAttachments() {
 		return attachments.size() > 0;
 	}
-	
+
 	// TODO short graph
 	@Override
 	public List<Attachment> getAttachments() {
 		return attachments;
 	}
-	
+
 	@Override
 	public void setAttachments(List<Attachment> attachments) {
 		this.attachments = attachments;
 	}
-	
+
 	@Override
 	public String getURL() {
 		return "p?id=" + this.getClass().getSimpleName().toLowerCase() + "-form&demandId=" + getIdentifier();
