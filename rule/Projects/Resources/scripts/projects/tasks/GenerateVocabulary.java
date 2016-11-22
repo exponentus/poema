9108b023efabf654df559c875194ea361ca59bbd
medin84 <kalihanovm@gmail.com>
2016-11-22 23:48:17 +0600

    gen_voc i18n

--- rule/Projects/Resources/scripts/projects/tasks/GenerateVocabulary.java
+++ rule/Projects/Resources/scripts/projects/tasks/GenerateVocabulary.java
@@ -1,23 +1,33 @@
 package projects.tasks;
 
-import java.io.File;
-
 import com.exponentus.appenv.AppEnv;
+import com.exponentus.env.Environment;
+import com.exponentus.localization.LanguageCode;
 import com.exponentus.localization.Vocabulary;
+import com.exponentus.localization.VocabularyDTO;
 import com.exponentus.scripting._Session;
 import com.exponentus.scripting.event._Do;
 import com.exponentus.scriptprocessor.tasks.Command;
 
+import java.io.File;
+
 @Command(name = "gen_voc")
 public class GenerateVocabulary extends _Do {
 
-	@Override
-	public void doTask(AppEnv appEnv, _Session ses) {
-		Vocabulary vocabulary = appEnv.vocabulary;
-		String jsFilePath = appEnv.getWebAppsPath() + File.separator + "vocabulary.js";
-		logger.infoLogEntry("write to \"" + jsFilePath + "\"");
-		writeJS(vocabulary, jsFilePath);
-		logger.infoLogEntry("done...");
-	}
+    @Override
+    public void doTask(AppEnv appEnv, _Session ses) {
+        Vocabulary vocabulary = appEnv.vocabulary;
 
+        String i18nPath = appEnv.getWebAppsPath() + File.separator + "i18n" + File.separator;
+        (new File(i18nPath)).mkdirs();
+
+        for (LanguageCode languageCode : Environment.langs) {
+            VocabularyDTO vocabularyDTO = VocabularyDTO.valueOf(vocabulary, languageCode);
+            String jsonFilePath = i18nPath + languageCode.name().toLowerCase() + ".json";
+            logger.infoLogEntry("write to \"" + jsonFilePath + "\"");
+            writeJS(vocabularyDTO.getWords(), jsonFilePath);
+        }
+
+        logger.infoLogEntry("done...");
+    }
 }
