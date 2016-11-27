73706edc17d51e8fa832b8a96f9a46ea64e89d70
kayra <justaidajam@gmail.com>
2016-11-27 22:08:05 +0200

    #

--- rule/Projects/Resources/scripts/projects/tasks/GenerateVocabulary.java
+++ rule/Projects/Resources/scripts/projects/tasks/GenerateVocabulary.java
@@ -1,33 +1,34 @@
 package projects.tasks;
 
+import java.io.File;
+
 import com.exponentus.appenv.AppEnv;
-import com.exponentus.env.Environment;
-import com.exponentus.localization.LanguageCode;
 import com.exponentus.localization.Vocabulary;
 import com.exponentus.localization.VocabularyDTO;
 import com.exponentus.scripting._Session;
 import com.exponentus.scripting.event._Do;
 import com.exponentus.scriptprocessor.tasks.Command;
 
-import java.io.File;
+import administrator.dao.LanguageDAO;
+import administrator.model.Language;
 
 @Command(name = "gen_voc")
 public class GenerateVocabulary extends _Do {
-
-    @Override
-    public void doTask(AppEnv appEnv, _Session ses) {
-        Vocabulary vocabulary = appEnv.vocabulary;
-
-        String i18nPath = appEnv.getWebAppsPath() + File.separator + "i18n" + File.separator;
-        (new File(i18nPath)).mkdirs();
-
-        for (LanguageCode languageCode : Environment.langs) {
-            VocabularyDTO vocabularyDTO = VocabularyDTO.valueOf(vocabulary, languageCode);
-            String jsonFilePath = i18nPath + languageCode.name().toLowerCase() + ".json";
-            logger.infoLogEntry("write to \"" + jsonFilePath + "\"");
-            writeJS(vocabularyDTO.getWords(), jsonFilePath);
-        }
-
-        logger.infoLogEntry("done...");
-    }
+	
+	@Override
+	public void doTask(AppEnv appEnv, _Session ses) {
+		Vocabulary vocabulary = appEnv.vocabulary;
+		
+		String i18nPath = appEnv.getWebAppsPath() + File.separator + "i18n" + File.separator;
+		(new File(i18nPath)).mkdirs();
+		LanguageDAO dao = new LanguageDAO();
+		for (Language lang : dao.findAll()) {
+			VocabularyDTO vocabularyDTO = VocabularyDTO.valueOf(vocabulary, lang.getCode());
+			String jsonFilePath = i18nPath + lang.getName().toLowerCase() + ".json";
+			logger.infoLogEntry("write to \"" + jsonFilePath + "\"");
+			writeJS(vocabularyDTO.getWords(), jsonFilePath);
+		}
+		
+		logger.infoLogEntry("done...");
+	}
 }
