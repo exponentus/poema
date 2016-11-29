1e31c72a8556d35baeb0f9622adb2c1d876cf0d4
kayra <justaidajam@gmail.com>
2016-11-29 13:04:42 +0200

    #

--- rule/Projects/Resources/scripts/projects/tasks/FillTags.java
+++ rule/Projects/Resources/scripts/projects/tasks/FillTags.java
@@ -1,26 +1,29 @@
-package projects.init;
+package projects.tasks;
 
 import java.util.ArrayList;
 import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
 
-import com.exponentus.dataengine.jpa.deploying.InitialDataAdapter;
+import com.exponentus.appenv.AppEnv;
+import com.exponentus.dataengine.exception.DAOException;
+import com.exponentus.dataengine.exception.DAOExceptionType;
+import com.exponentus.exception.SecureException;
 import com.exponentus.localization.LanguageCode;
-import com.exponentus.localization.Vocabulary;
 import com.exponentus.scripting._Session;
+import com.exponentus.scripting.event._Do;
+import com.exponentus.scriptprocessor.tasks.Command;
 
 import reference.dao.TagDAO;
 import reference.model.Tag;
 
-/**
- * Created by Kayra on 28/01/16.
- */
-
-public class FillTags extends InitialDataAdapter<Tag, TagDAO> {
+@Command(name = "fill_tags")
+public class FillTags extends _Do {
+	private static final String CATEGORY_NAME = "software_developing";
 	
 	@Override
-	public List<Tag> getData(_Session ses, LanguageCode lang, Vocabulary vocabulary) {
+	public void doTask(AppEnv appEnv, _Session ses) {
+
 		List<Tag> entities = new ArrayList<Tag>();
 		
 		Tag entity = new Tag();
@@ -31,6 +34,7 @@ public class FillTags extends InitialDataAdapter<Tag, TagDAO> {
 		name.put(LanguageCode.POR, "Estrelou");
 		entity.setLocalizedName(name);
 		entity.setColor("#008080");
+		entity.setCategory(CATEGORY_NAME);
 		entities.add(entity);
 		
 		entity = new Tag();
@@ -41,6 +45,7 @@ public class FillTags extends InitialDataAdapter<Tag, TagDAO> {
 		name.put(LanguageCode.POR, "Erro");
 		entity.setLocalizedName(name);
 		entity.setColor("#ff0000");
+		entity.setCategory(CATEGORY_NAME);
 		entities.add(entity);
 		
 		entity = new Tag();
@@ -51,6 +56,7 @@ public class FillTags extends InitialDataAdapter<Tag, TagDAO> {
 		name.put(LanguageCode.POR, "Aprimoramento");
 		entity.setLocalizedName(name);
 		entity.setColor("#0080ff");
+		entity.setCategory(CATEGORY_NAME);
 		entities.add(entity);
 		
 		entity = new Tag();
@@ -61,10 +67,33 @@ public class FillTags extends InitialDataAdapter<Tag, TagDAO> {
 		name.put(LanguageCode.KAZ, "Mерзімі өткен");
 		entity.setLocalizedName(name);
 		entity.setColor("#db0000");
+		entity.setCategory(CATEGORY_NAME);
 		entity.setHidden(true);
 		entities.add(entity);
-		
-		return entities;
+
+		try {
+			TagDAO dao = new TagDAO(ses);
+			for (Tag entry : entities) {
+				try {
+					if (dao.add(entry) != null) {
+						logger.infoLogEntry(entry.getName() + " added");
+					}
+				} catch (DAOException e) {
+					if (e.getType() == DAOExceptionType.UNIQUE_VIOLATION) {
+						logger.warningLogEntry("a data is already exists (" + e.getAddInfo() + "), record was skipped");
+					} else if (e.getType() == DAOExceptionType.NOT_NULL_VIOLATION) {
+						logger.warningLogEntry("a value is null (" + e.getAddInfo() + "), record was skipped");
+					} else {
+						logger.errorLogEntry(e);
+					}
+				} catch (SecureException e) {
+					logger.errorLogEntry(e);
+				}
+			}
+		} catch (DAOException e) {
+			logger.errorLogEntry(e);
+		}
+		logger.infoLogEntry("done...");
 	}
 	
 }
