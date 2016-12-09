0b043ceb0fd073b280ade4b4b6b0a69801fe7c86
kayra <justaidajam@gmail.com>
2016-12-09 21:20:23 +0200

    #

--- rule/Workflow/Resources/scripts/workflow/model/Incoming.java
+++ rule/Workflow/Resources/scripts/workflow/model/Incoming.java
@@ -43,40 +43,39 @@ import staff.model.Organization;
 @Table(name = "incomings")
 @NamedQuery(name = "Incoming.findAll", query = "SELECT m FROM Incoming AS m ORDER BY m.regDate")
 public class Incoming extends HierarchicalEntity<UUID> {
-
+	
 	@JsonIgnore
 	@OneToMany(mappedBy = "incoming", fetch = FetchType.LAZY)
 	private List<Assignment> assignments;
-
+	
 	@Column(name = "reg_number", unique = true)
 	private String regNumber;
-
+	
 	@Column(name = "applied_reg_date")
 	private Date appliedRegDate;
-
+	
 	private Organization sender;
-
+	
 	@Column(name = "sender_reg_number")
 	private String senderRegNumber;
-
+	
 	@Column(name = "sender_applied_reg_date")
 	private Date senderAppliedRegDate;
-
-	@Column(nullable = false)
+	
 	protected Employee addressee;
-
+	
 	private DocumentLanguage docLanguage;
-
+	
 	private DocumentType docType;
-
+	
 	private DocumentSubject docSubject;
-
+	
 	private Outgoing responseTo;
-
+	
 	@FTSearchable
 	@Column(columnDefinition = "TEXT")
 	private String body;
-
+	
 	@OneToMany(fetch = FetchType.EAGER, cascade = CascadeType.ALL, orphanRemoval = true)
 	@JoinTable(name = "incoming_attachments", joinColumns = {
 			@JoinColumn(name = "incoming_id") }, inverseJoinColumns = {
@@ -85,38 +84,38 @@ public class Incoming extends HierarchicalEntity<UUID> {
 									"incoming_id", "attachment_id" }))
 	@CascadeOnDelete
 	private List<Attachment> attachments = new ArrayList<>();
-
+	
 	@ManyToMany(fetch = FetchType.EAGER)
 	@JoinTable(name = "incoming_tags")
 	private List<Tag> tags;
-
+	
 	@Transient
 	private List<IAppEntity> responses;
-
+	
 	public String getRegNumber() {
 		return regNumber;
 	}
-
+	
 	public void setRegNumber(String regNumber) {
 		this.regNumber = regNumber;
 	}
-
+	
 	public Date getAppliedRegDate() {
 		return appliedRegDate;
 	}
-
+	
 	public void setAppliedRegDate(Date appliedRegDate) {
 		this.appliedRegDate = appliedRegDate;
 	}
-
+	
 	public Organization getSender() {
 		return sender;
 	}
-
+	
 	public void setSender(Organization sender) {
 		this.sender = sender;
 	}
-
+	
 	@JsonSetter("sender")
 	public void setSenderId(UUID id) {
 		if (id != null) {
@@ -124,49 +123,49 @@ public class Incoming extends HierarchicalEntity<UUID> {
 			sender.setId(id);
 		}
 	}
-
+	
 	public String getSenderRegNumber() {
 		return senderRegNumber;
 	}
-
+	
 	public void setSenderRegNumber(String senderRegNumber) {
 		this.senderRegNumber = senderRegNumber;
 	}
-
+	
 	public Date getSenderAppliedRegDate() {
 		return senderAppliedRegDate;
 	}
-
+	
 	public void setSenderAppliedRegDate(Date senderAppliedRegDate) {
 		this.senderAppliedRegDate = senderAppliedRegDate;
 	}
-
+	
 	public Employee getAddressee() {
 		return addressee;
 	}
-
+	
 	public void setAddressee(Employee addressee) {
 		this.addressee = addressee;
 	}
-
+	
 	@Override
 	public List<Attachment> getAttachments() {
 		return attachments;
 	}
-
+	
 	@Override
 	public void setAttachments(List<Attachment> attachments) {
 		this.attachments = attachments;
 	}
-
+	
 	public DocumentLanguage getDocLanguage() {
 		return docLanguage;
 	}
-
+	
 	public void setDocLanguage(DocumentLanguage docLanguage) {
 		this.docLanguage = docLanguage;
 	}
-
+	
 	@JsonSetter("docLanguage")
 	public void setDocLanguageId(UUID id) {
 		if (id != null) {
@@ -174,23 +173,23 @@ public class Incoming extends HierarchicalEntity<UUID> {
 			docLanguage.setId(id);
 		}
 	}
-
+	
 	public DocumentType getDocType() {
 		return docType;
 	}
-
+	
 	public void setDocType(DocumentType docType) {
 		this.docType = docType;
 	}
-
+	
 	public DocumentSubject getDocSubject() {
 		return docSubject;
 	}
-
+	
 	public void setDocSubject(DocumentSubject docSubject) {
 		this.docSubject = docSubject;
 	}
-
+	
 	@JsonSetter("docType")
 	public void setDocTypeId(UUID id) {
 		if (id != null) {
@@ -198,15 +197,15 @@ public class Incoming extends HierarchicalEntity<UUID> {
 			docType.setId(id);
 		}
 	}
-
+	
 	public Outgoing getResponseTo() {
 		return responseTo;
 	}
-
+	
 	public void setResponseTo(Outgoing responseTo) {
 		this.responseTo = responseTo;
 	}
-
+	
 	@JsonSetter("responseTo")
 	public void setResponseToId(UUID id) {
 		if (id != null) {
@@ -214,36 +213,36 @@ public class Incoming extends HierarchicalEntity<UUID> {
 			responseTo.setId(id);
 		}
 	}
-
+	
 	public String getBody() {
 		return body;
 	}
-
+	
 	public void setBody(String body) {
 		this.body = body;
 	}
-
+	
 	public List<Tag> getTags() {
 		return tags;
 	}
-
+	
 	public void setTags(List<Tag> tags) {
 		this.tags = tags;
 	}
-
+	
 	@Override
 	public String getURL() {
 		return "incomings/" + getIdentifier();
 	}
-
+	
 	public void setAssignments(List<Assignment> assignments) {
 		this.assignments = assignments;
 	}
-
+	
 	public List<IAppEntity> getResponses() {
 		return responses;
 	}
-
+	
 	public void setResponses(List<IAppEntity> responses) {
 		this.responses = responses;
 	}
