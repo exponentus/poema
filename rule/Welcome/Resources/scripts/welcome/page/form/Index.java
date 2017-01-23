0712f83be78d54c17f7bee7662540d16a7319b0a
kayra <justaidajam@gmail.com>
2017-01-24 00:32:42 +0200

    #

--- rule/Welcome/Resources/scripts/welcome/page/form/Index.java
+++ rule/Welcome/Resources/scripts/welcome/page/form/Index.java
@@ -1,8 +1,5 @@
 package welcome.page.form;
 
-import java.util.ArrayList;
-import java.util.List;
-
 import com.exponentus.localization.LanguageCode;
 import com.exponentus.messaging.MessageType;
 import com.exponentus.scripting._Session;
@@ -10,7 +7,6 @@ import com.exponentus.scripting._WebFormData;
 import com.exponentus.scripting.event._DoPage;
 
 import administrator.dao.LanguageDAO;
-import administrator.model.Language;
 
 public class Index extends _DoPage {
 
@@ -26,23 +22,14 @@ public class Index extends _DoPage {
 			} else if (session.getLang() == null) {
 				session.setLang(LanguageCode.ENG);
 			}
-			addValue("template",getCurrentAppEnv().templates.getTemplate(MessageType.SITE, "faq", l));
+			addValue("template", getCurrentAppEnv().templates.getTemplate(MessageType.SITE, "faq", l));
 		} catch (Exception e) {
 			addContent("error", "the " + toLang + " language is not available");
 		}
 
 		LanguageDAO lDao = new LanguageDAO(session);
-		List<Language> lList = new ArrayList<>();
-		try {
-			lList.add(lDao.findByCode(LanguageCode.ENG));
-			lList.add(lDao.findByCode(LanguageCode.BUL));
-			lList.add(lDao.findByCode(LanguageCode.RUS));
-			lList.add(lDao.findByCode(LanguageCode.SPA));
-			lList.add(lDao.findByCode(LanguageCode.POR));
-			lList.add(lDao.findByCode(LanguageCode.KAZ));
-		} catch (Exception e) {
-		}
-		addContent(lList);
+
+		addContent(lDao.findAllActivated());
 	}
 
 }
