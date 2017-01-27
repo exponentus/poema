8c613c3569448a339d61a11fe7a5e41093e71167
medin84 <medin_84@mail.ru>
2017-01-27 16:45:43 +0600

    commit

--- rule/Workflow/Resources/scripts/workflow/services/officememo/OfficeMemoService.java
+++ rule/Workflow/Resources/scripts/workflow/services/officememo/OfficeMemoService.java
@@ -1,21 +1,5 @@
 package workflow.services.officememo;
 
-import java.util.Arrays;
-import java.util.List;
-import java.util.UUID;
-import java.util.stream.Collectors;
-
-import javax.ws.rs.Consumes;
-import javax.ws.rs.DELETE;
-import javax.ws.rs.GET;
-import javax.ws.rs.HttpMethod;
-import javax.ws.rs.POST;
-import javax.ws.rs.Path;
-import javax.ws.rs.PathParam;
-import javax.ws.rs.Produces;
-import javax.ws.rs.core.MediaType;
-import javax.ws.rs.core.Response;
-
 import com.exponentus.common.model.ACL;
 import com.exponentus.dataengine.exception.DAOException;
 import com.exponentus.dataengine.jpa.ViewPage;
@@ -33,216 +17,235 @@ import com.exponentus.scripting.actions._Action;
 import com.exponentus.scripting.actions._ActionBar;
 import com.exponentus.scripting.actions._ActionType;
 import com.exponentus.user.IUser;
-
 import workflow.dao.OfficeMemoDAO;
 import workflow.model.OfficeMemo;
+import workflow.model.constants.ApprovalStatusType;
+import workflow.model.constants.ApprovalType;
+import workflow.model.embedded.Approval;
+import workflow.model.embedded.Block;
+
+import javax.ws.rs.*;
+import javax.ws.rs.core.MediaType;
+import javax.ws.rs.core.Response;
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+import java.util.UUID;
+import java.util.stream.Collectors;
 
 @Path("office-memos")
 public class OfficeMemoService extends RestProvider {
 
-	private Outcome outcome = new Outcome();
+    private Outcome outcome = new Outcome();
 
-	@GET
-	@Produces(MediaType.APPLICATION_JSON)
-	public Response getView() {
-		_Session session = getSession();
-		int pageSize = session.pageSize;
-		_SortParams sortParams = getWebFormData().getSortParams(_SortParams.desc("regDate"));
-		String[] expandedIds = getWebFormData().getListOfValuesSilently("expandedIds");
-		List<UUID> expandedIdList = Arrays.stream(expandedIds).map(UUID::fromString).collect(Collectors.toList());
-		try {
-			OfficeMemoDAO officeMemoDAO = new OfficeMemoDAO(session);
-			ViewPage vp = officeMemoDAO.findViewPage(sortParams, getWebFormData().getPage(), pageSize);
+    @GET
+    @Produces(MediaType.APPLICATION_JSON)
+    public Response getView() {
+        _Session session = getSession();
+        int pageSize = session.pageSize;
+        _SortParams sortParams = getWebFormData().getSortParams(_SortParams.desc("regDate"));
+        String[] expandedIds = getWebFormData().getListOfValuesSilently("expandedIds");
+        List<UUID> expandedIdList = Arrays.stream(expandedIds).map(UUID::fromString).collect(Collectors.toList());
+        try {
+            OfficeMemoDAO officeMemoDAO = new OfficeMemoDAO(session);
+            ViewPage vp = officeMemoDAO.findViewPage(sortParams, getWebFormData().getPage(), pageSize);
 
-			//
-			_ActionBar actionBar = new _ActionBar(session);
-			actionBar.addAction(new _Action("add_new", "", "new_office_memo"));
-			actionBar.addAction(new _Action("", "", "refresh", "fa fa-refresh"));
-			// actionBar.addAction(new _Action("del_document", "", _ActionType.DELETE_DOCUMENT));
+            //
+            _ActionBar actionBar = new _ActionBar(session);
+            actionBar.addAction(new _Action("add_new", "", "new_office_memo"));
+            actionBar.addAction(new _Action("", "", "refresh", "fa fa-refresh"));
+            // actionBar.addAction(new _Action("del_document", "", _ActionType.DELETE_DOCUMENT));
 
-			outcome.setId("office-memo");
-			outcome.setTitle("office_memo_plural");
-			outcome.addPayload(actionBar);
-			outcome.addPayload(vp);
+            outcome.setId("office-memo");
+            outcome.setTitle("office_memo_plural");
+            outcome.addPayload(actionBar);
+            outcome.addPayload(vp);
 
-			return Response.ok(outcome).build();
-		} catch (DAOException e) {
-			return responseException(e);
-		}
-	}
+            return Response.ok(outcome).build();
+        } catch (DAOException e) {
+            return responseException(e);
+        }
+    }
 
-	@GET
-	@Path("{id}")
-	@Produces(MediaType.APPLICATION_JSON)
-	public Response getById(@PathParam("id") String id) {
-		_Session ses = getSession();
-		OfficeMemo entity;
-		try {
-			boolean isNew = "new".equals(id);
-			if (isNew) {
-				entity = new OfficeMemo();
-				// Approval approval = new Approval();
-				// approval.setStatus(ApprovalStatusType.DRAFT);
-				// Block block = new Block();
-				// block.setStatus(ApprovalStatusType.DRAFT);
-				// block.setType(ApprovalType.SERIAL);
-				// List<Block> blocks = new ArrayList<Block>();
-				// blocks.add(block);
-				// approval.setBlocks(blocks);
-				// entity.setApproval(approval);
-				entity.setAppliedAuthor(ses.getUser().getId());
-			} else {
-				OfficeMemoDAO officeMemoDAO = new OfficeMemoDAO(ses);
-				entity = officeMemoDAO.findById(id);
-			}
-			
-			outcome.setId(id);
-			outcome.addPayload(entity);
-			outcome.addPayload(getActionBar(ses, entity));
-			outcome.addPayload(EnvConst.FSID_FIELD_NAME, getWebFormData().getFormSesId());
-			if (!isNew) {
-				outcome.addPayload(new ACL(entity));
-			}
-			
-			return Response.ok(outcome).build();
-		} catch (DAOException e) {
-			return responseException(e);
-		}
-	}
+    @GET
+    @Path("{id}")
+    @Produces(MediaType.APPLICATION_JSON)
+    public Response getById(@PathParam("id") String id) {
+        _Session ses = getSession();
+        OfficeMemo entity;
+        try {
+            boolean isNew = "new".equals(id);
+            if (isNew) {
+                entity = new OfficeMemo();
+                Approval approval = new Approval();
+                approval.setStatus(ApprovalStatusType.DRAFT);
+                //
+                Block block = new Block();
+                block.setStatus(ApprovalStatusType.DRAFT);
+                block.setType(ApprovalType.SERIAL);
+                List<Block> blocks = new ArrayList<Block>();
+                blocks.add(block);
 
-	@POST
-	@Path("{id}")
-	@Produces(MediaType.APPLICATION_JSON)
-	@Consumes(MediaType.APPLICATION_JSON)
-	public Response save(@PathParam("id") String id, OfficeMemo form) {
-		_Validation validation = validate(form);
-		if (validation.hasError()) {
-			return responseValidationError(validation);
-		}
+                Block block2 = new Block();
+                block2.setStatus(ApprovalStatusType.DRAFT);
+                block2.setType(ApprovalType.SERIAL);
+                blocks.add(block2);
+                //
+                approval.setBlocks(blocks);
+                entity.setApproval(approval);
+                entity.setAppliedAuthor(ses.getUser().getId());
+            } else {
+                OfficeMemoDAO officeMemoDAO = new OfficeMemoDAO(ses);
+                entity = officeMemoDAO.findById(id);
+            }
 
-		_Session ses = getSession();
-		try {
-			OfficeMemoDAO officeMemoDAO = new OfficeMemoDAO(ses);
-			OfficeMemo entity;
+            outcome.setId(id);
+            outcome.addPayload(entity);
+            outcome.addPayload(getActionBar(ses, entity));
+            outcome.addPayload(EnvConst.FSID_FIELD_NAME, getWebFormData().getFormSesId());
+            if (!isNew) {
+                outcome.addPayload(new ACL(entity));
+            }
 
-			boolean isNew = "new".equals(id);
-			if (isNew) {
-				entity = new OfficeMemo();
-			} else {
-				entity = officeMemoDAO.findById(id);
-			}
+            return Response.ok(outcome).build();
+        } catch (DAOException e) {
+            return responseException(e);
+        }
+    }
 
-			//
-			entity.setAppliedRegDate(form.getAppliedRegDate());
-			if (form.getApproval() != null) {
-				// entity.setApproval(approvalDAO.findById(form.getApproval().getId()));
-				entity.setApproval(form.getApproval());
-			} else {
-				entity.setApproval(null);
-			}
-			entity.setTitle(form.getTitle());
-			entity.setBody(form.getBody());
-			entity.setRecipient(form.getRecipient());
-			entity.setAttachments(getActualAttachments(entity.getAttachments()));
+    @POST
+    @Path("{id}")
+    @Produces(MediaType.APPLICATION_JSON)
+    @Consumes(MediaType.APPLICATION_JSON)
+    public Response save(@PathParam("id") String id, OfficeMemo form) {
+        _Validation validation = validate(form);
+        if (validation.hasError()) {
+            return responseValidationError(validation);
+        }
 
-			if (isNew) {
-				RegNum rn = new RegNum();
-				entity.setRegNumber(Integer.toString(rn.getRegNumber(entity.getDefaultFormName())));
+        _Session ses = getSession();
+        try {
+            OfficeMemoDAO officeMemoDAO = new OfficeMemoDAO(ses);
+            OfficeMemo entity;
 
-				IUser<Long> user = ses.getUser();
-				entity.addReaderEditor(user);
-				entity.setAppliedAuthor(ses.getUser().getId());
-				entity = officeMemoDAO.add(entity);
-			} else {
-				entity = officeMemoDAO.update(entity);
-			}
+            boolean isNew = "new".equals(id);
+            if (isNew) {
+                entity = new OfficeMemo();
+            } else {
+                entity = officeMemoDAO.findById(id);
+            }
 
-			outcome.setId(id);
-			outcome.setTitle(entity.getTitle());
-			outcome.addPayload(entity);
+            //
+            entity.setAppliedRegDate(form.getAppliedRegDate());
+            if (form.getApproval() != null) {
+                // entity.setApproval(approvalDAO.findById(form.getApproval().getId()));
+                entity.setApproval(form.getApproval());
+            } else {
+                entity.setApproval(null);
+            }
+            entity.setTitle(form.getTitle());
+            entity.setBody(form.getBody());
+            entity.setRecipient(form.getRecipient());
+            entity.setAttachments(getActualAttachments(entity.getAttachments()));
 
-			return Response.ok(outcome).build();
-		} catch (SecureException | DAOException e) {
-			return responseException(e);
-		}
-	}
+            if (isNew) {
+                RegNum rn = new RegNum();
+                entity.setRegNumber(Integer.toString(rn.getRegNumber(entity.getDefaultFormName())));
 
-	@DELETE
-	@Path("{id}")
-	@Produces(MediaType.APPLICATION_JSON)
-	public Response delete(@PathParam("id") String id) {
-		_Session ses = getSession();
-		try {
-			OfficeMemoDAO dao = new OfficeMemoDAO(ses);
-			OfficeMemo entity = dao.findById(id);
-			if (entity != null) {
-				try {
-					dao.delete(entity);
-				} catch (SecureException | DAOException e) {
-					return responseException(e);
-				}
-			}
-			return Response.noContent().build();
-		} catch (DAOException e) {
-			return responseException(e);
-		}
-	}
+                IUser<Long> user = ses.getUser();
+                entity.addReaderEditor(user);
+                entity.setAppliedAuthor(ses.getUser().getId());
+                entity = officeMemoDAO.add(entity);
+            } else {
+                entity = officeMemoDAO.update(entity);
+            }
 
-	/*
-	 * Get entity attachment or _thumbnail
-	 */
-	@GET
-	@Path("{id}/attachments/{attachId}")
-	@Produces(MediaType.APPLICATION_OCTET_STREAM)
-	public Response getAttachment(@PathParam("id") String id, @PathParam("attachId") String attachId) {
-		try {
-			OfficeMemoDAO dao = new OfficeMemoDAO(getSession());
-			OfficeMemo entity = dao.findById(id);
-			
-			return getAttachment(entity, attachId);
-		} catch (DAOException e) {
-			return responseException(e);
-		}
-	}
+            outcome.setId(id);
+            outcome.setTitle(entity.getTitle());
+            outcome.addPayload(entity);
 
-	@DELETE
-	@Path("{id}/attachments/{attachmentId}")
-	public Response deleteAttachment(@PathParam("id") String id, @PathParam("attachmentId") String attachmentId) {
-		return deleteAttachmentFromSessionFormAttachments(attachmentId);
-	}
+            return Response.ok(outcome).build();
+        } catch (SecureException | DAOException e) {
+            return responseException(e);
+        }
+    }
 
-	/*
-	 *
-	 */
-	private _ActionBar getActionBar(_Session session, OfficeMemo entity) {
-		_ActionBar actionBar = new _ActionBar(session);
+    @DELETE
+    @Path("{id}")
+    @Produces(MediaType.APPLICATION_JSON)
+    public Response delete(@PathParam("id") String id) {
+        _Session ses = getSession();
+        try {
+            OfficeMemoDAO dao = new OfficeMemoDAO(ses);
+            OfficeMemo entity = dao.findById(id);
+            if (entity != null) {
+                try {
+                    dao.delete(entity);
+                } catch (SecureException | DAOException e) {
+                    return responseException(e);
+                }
+            }
+            return Response.noContent().build();
+        } catch (DAOException e) {
+            return responseException(e);
+        }
+    }
 
-		actionBar.addAction(new _Action("close", "", _ActionType.CLOSE));
-		actionBar.addAction(new _Action("save_close", "", _ActionType.SAVE_AND_CLOSE));
-		if (!entity.isNew() && entity.isEditable()) {
-			actionBar.addAction(new _Action("delete", "", _ActionType.DELETE_DOCUMENT));
-		}
+    /*
+     * Get entity attachment or _thumbnail
+     */
+    @GET
+    @Path("{id}/attachments/{attachId}")
+    @Produces(MediaType.APPLICATION_OCTET_STREAM)
+    public Response getAttachment(@PathParam("id") String id, @PathParam("attachId") String attachId) {
+        try {
+            OfficeMemoDAO dao = new OfficeMemoDAO(getSession());
+            OfficeMemo entity = dao.findById(id);
 
-		return actionBar;
-	}
+            return getAttachment(entity, attachId);
+        } catch (DAOException e) {
+            return responseException(e);
+        }
+    }
 
-	private _Validation validate(OfficeMemo entity) {
-		_Validation ve = new _Validation();
+    @DELETE
+    @Path("{id}/attachments/{attachmentId}")
+    public Response deleteAttachment(@PathParam("id") String id, @PathParam("attachmentId") String attachmentId) {
+        return deleteAttachmentFromSessionFormAttachments(attachmentId);
+    }
 
-		if (entity.getTitle() == null || entity.getTitle().isEmpty()) {
-			ve.addError("title", "required", "field_is_empty");
-		}
+    /*
+     *
+     */
+    private _ActionBar getActionBar(_Session session, OfficeMemo entity) {
+        _ActionBar actionBar = new _ActionBar(session);
 
-		return ve;
-	}
+        actionBar.addAction(new _Action("close", "", _ActionType.CLOSE));
+        actionBar.addAction(new _Action("save_close", "", _ActionType.SAVE_AND_CLOSE));
+        if (!entity.isNew() && entity.isEditable()) {
+            actionBar.addAction(new _Action("delete", "", _ActionType.DELETE_DOCUMENT));
+        }
 
-	@Override
-	public ServiceDescriptor updateDescription(ServiceDescriptor sd) {
-		sd.setName(getClass().getName());
-		ServiceMethod m = new ServiceMethod();
-		m.setMethod(HttpMethod.GET);
-		m.setURL("/" + sd.getAppName() + sd.getUrlMapping());
-		sd.addMethod(m);
-		return sd;
-	}
+        return actionBar;
+    }
+
+    private _Validation validate(OfficeMemo entity) {
+        _Validation ve = new _Validation();
+
+        if (entity.getTitle() == null || entity.getTitle().isEmpty()) {
+            ve.addError("title", "required", "field_is_empty");
+        }
+
+        return ve;
+    }
+
+    @Override
+    public ServiceDescriptor updateDescription(ServiceDescriptor sd) {
+        sd.setName(getClass().getName());
+        ServiceMethod m = new ServiceMethod();
+        m.setMethod(HttpMethod.GET);
+        m.setURL("/" + sd.getAppName() + sd.getUrlMapping());
+        sd.addMethod(m);
+        return sd;
+    }
 }
