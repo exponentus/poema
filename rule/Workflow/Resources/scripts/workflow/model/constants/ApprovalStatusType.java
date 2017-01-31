32ec0a8841ff5b378b8ec40e6c68775851734966
kayra <justaidajam@gmail.com>
2017-01-31 13:58:56 +0200

    #

--- rule/Workflow/Resources/scripts/workflow/model/constants/ApprovalStatusType.java
+++ rule/Workflow/Resources/scripts/workflow/model/constants/ApprovalStatusType.java
@@ -5,18 +5,18 @@ package workflow.model.constants;
  * @author Kayra created 07-04-2016
  */
 public enum ApprovalStatusType {
-	UNKNOWN(0), DRAFT(80), PROCESSING(81), FINISHED(82), AWAITONG(83);
-	
+	UNKNOWN(0), DRAFT(80), PROCESSING(81), FINISHED(82), AWAITING(83);
+
 	private int code;
-	
+
 	ApprovalStatusType(int code) {
 		this.code = code;
 	}
-	
+
 	public int getCode() {
 		return code;
 	}
-	
+
 	public static ApprovalStatusType getType(int code) {
 		for (ApprovalStatusType type : values()) {
 			if (type.code == code) {
@@ -25,5 +25,5 @@ public enum ApprovalStatusType {
 		}
 		return UNKNOWN;
 	}
-	
+
 }
