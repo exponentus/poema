5291d972d9c9c51c55982f55b4cca8565b04b7eb
medin84 <medin_84@mail.ru>
2017-04-05 19:27:26 +0600

    commit

--- rule/Workflow/Resources/scripts/workflow/domain/impl/ApplicationForVehicleDomain.java
+++ rule/Workflow/Resources/scripts/workflow/domain/impl/ApplicationForVehicleDomain.java
@@ -1,5 +1,6 @@
 package workflow.domain.impl;
 
+import com.exponentus.common.model.ACL;
 import com.exponentus.rest.outgoingdto.Outcome;
 import staff.model.Employee;
 import workflow.domain.IApplicationForVehicleDomain;
@@ -14,11 +15,47 @@ public class ApplicationForVehicleDomain implements IApplicationForVehicleDomain
 
     @Override
     public void fillFromDto(ApplicationForVehicle entity, ApplicationForVehicle dto, Employee author) {
+        if (entity.isNew()) {
+            entity.setAuthor(author.getUser());
 
+            entity.addReaderEditor(entity.getAuthor());
+            if (dto.getAppliedAuthor() != null) {
+                entity.addReaderEditor(dto.getAppliedAuthor().getUser());
+            }
+        }
+
+        entity.setTitle(dto.getTitle());
+        entity.setBody(dto.getBody());
+        entity.setAppliedAuthor(dto.getAppliedAuthor());
+        entity.setAppliedRegDate(dto.getAppliedRegDate());
+        entity.setRecipient(dto.getRecipient());
+        entity.setVehicle(dto.getVehicle());
+        entity.setUseFrom(dto.getUseFrom());
+        entity.setUseTo(dto.getUseTo());
+        entity.setRoute(dto.getRoute());
+        entity.setTags(dto.getTags());
+        entity.setAttachments(dto.getAttachments());
+
+        if (dto.getApproval() != null) {
+            if (entity.isNew()) {
+                dto.getApproval().setVersion(1);
+            }
+            entity.setApproval(dto.getApproval());
+        } else {
+            entity.setApproval(null);
+        }
     }
 
     @Override
     public Outcome getOutcome(ApplicationForVehicle entity) {
-        return null;
+        Outcome outcome = new Outcome();
+
+        outcome.setTitle(entity.getTitle());
+        outcome.addPayload(entity);
+        if (!entity.isNew()) {
+            outcome.addPayload(new ACL(entity));
+        }
+
+        return outcome;
     }
 }
