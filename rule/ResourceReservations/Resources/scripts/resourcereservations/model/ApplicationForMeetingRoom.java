094dde9824cfdb3b5fcbf9ff8000a1da0fb6ee62
kaira <biosubj@gmail.com>
2017-04-13 01:54:52 +0300

    #

---
+++ rule/ResourceReservations/Resources/scripts/resourcereservations/model/ApplicationForMeetingRoom.java
@@ -0,0 +1,142 @@
+package resourcereservations.model;
+
+import java.util.Date;
+import java.util.List;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.FetchType;
+import javax.persistence.JoinColumn;
+import javax.persistence.JoinTable;
+import javax.persistence.ManyToMany;
+import javax.persistence.ManyToOne;
+import javax.persistence.Table;
+
+import com.exponentus.dataengine.jpadatabase.ftengine.FTSearchable;
+import com.fasterxml.jackson.annotation.JsonRootName;
+
+import administrator.model.User;
+import reference.model.MeetingRoom;
+import reference.model.Tag;
+import staff.model.Employee;
+import workflow.model.embedded.ApprovalSecureAppEntity;
+
+@JsonRootName("applicationForMeetingRoom")
+@Entity
+@Table(name = "applications_for_meeting_room")
+public class ApplicationForMeetingRoom extends ApprovalSecureAppEntity {
+
+	@Column(name = "reg_number")
+	private String regNumber;
+
+	@Column(name = "applied_reg_date")
+	private Date appliedRegDate;
+
+	@JoinColumn(name = "applied_author", nullable = false)
+	private Employee appliedAuthor;
+
+	@JoinColumn(nullable = false)
+	private Employee recipient;
+
+	@ManyToOne(optional = true)
+	@JoinColumn(nullable = false)
+	private MeetingRoom room;
+
+	@FTSearchable
+	@Column(columnDefinition = "TEXT")
+	private String body;
+
+	@FTSearchable
+	@Column(name = "use_from")
+	private Date useFrom;
+
+	@FTSearchable
+	@Column(name = "use_to")
+	private Date useTo;
+
+	@FTSearchable
+	private String location;
+
+	private List<User> observers;
+
+	@ManyToMany(fetch = FetchType.EAGER)
+	@JoinTable(name = "application_for_vehicle_tags")
+	private List<Tag> tags;
+
+	public String getRegNumber() {
+		return regNumber;
+	}
+
+	public void setRegNumber(String regNumber) {
+		this.regNumber = regNumber;
+	}
+
+	public Date getAppliedRegDate() {
+		return appliedRegDate;
+	}
+
+	public void setAppliedRegDate(Date appliedRegDate) {
+		this.appliedRegDate = appliedRegDate;
+	}
+
+	public Employee getAppliedAuthor() {
+		return appliedAuthor;
+	}
+
+	public void setAppliedAuthor(Employee appliedAuthor) {
+		this.appliedAuthor = appliedAuthor;
+	}
+
+	public Employee getRecipient() {
+		return recipient;
+	}
+
+	public void setRecipient(Employee recipient) {
+		this.recipient = recipient;
+	}
+
+	public String getBody() {
+		return body;
+	}
+
+	public void setBody(String body) {
+		this.body = body;
+	}
+
+	public Date getUseFrom() {
+		return useFrom;
+	}
+
+	public void setUseFrom(Date useFrom) {
+		this.useFrom = useFrom;
+	}
+
+	public Date getUseTo() {
+		return useTo;
+	}
+
+	public void setUseTo(Date useTo) {
+		this.useTo = useTo;
+	}
+
+	public List<User> getObservers() {
+		return observers;
+	}
+
+	public void setObservers(List<User> observers) {
+		this.observers = observers;
+	}
+
+	public List<Tag> getTags() {
+		return tags;
+	}
+
+	public void setTags(List<Tag> tags) {
+		this.tags = tags;
+	}
+
+	@Override
+	public String getURL() {
+		return "applications_for_vehicle/" + getIdentifier();
+	}
+}
