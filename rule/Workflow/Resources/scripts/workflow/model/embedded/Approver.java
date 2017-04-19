c2a917e0bdbfb8e859b5d3662e7e2fb721e2ad2a
kaira <biosubj@gmail.com>
2017-04-19 17:37:36 +0300

    #

--- rule/Workflow/Resources/scripts/workflow/model/embedded/Approver.java
+++ rule/Workflow/Resources/scripts/workflow/model/embedded/Approver.java
@@ -22,7 +22,7 @@ import workflow.model.constants.converter.DecisionTypeConverter;
  */
 
 @Entity
-@Table(name = "approvers")
+@Table(name = "wf_approvers")
 public class Approver extends SimpleAppEntity {
 
 	@JoinColumn(nullable = false)
