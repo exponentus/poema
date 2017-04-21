80d1b12ec40a66d2d44c570bf97a865384271263
medin84 <medin_84@mail.ru>
2017-04-22 00:23:30 +0600

    act

--- rule/Workflow/Resources/scripts/workflow/constants/Action.java
+++ rule/Workflow/Resources/scripts/workflow/constants/Action.java
@@ -12,13 +12,13 @@ public class Action {
     public static final _Action newOutgoing = new _Action(_ActionType.LINK).caption("outgoing").url("outgoings/new");
     public static final _Action newOfficeMemo = new _Action(_ActionType.LINK).caption("office_memo").url("office-memos/new");
 
-    public static final _Action startApproving = new _Action(_ActionType.API_ACTION).caption("start_approving").url("startApproving");
-    public static final _Action signApprovalBlock = new _Action(_ActionType.API_ACTION).caption("sign").url("acceptApprovalBlock");
-    public static final _Action acceptApprovalBlock = new _Action(_ActionType.API_ACTION).caption("accept").url("acceptApprovalBlock");
-    public static final _Action declineApprovalBlock = new _Action(_ActionType.API_ACTION).caption("decline").url("declineApprovalBlock");
+    public static final _Action startApproving = new _Action(_ActionType.API_ACTION).id("startApproving").caption("start_approving").url("startApproving");
+    public static final _Action signApprovalBlock = new _Action(_ActionType.API_ACTION).id("acceptApprovalBlock").caption("sign").url("acceptApprovalBlock");
+    public static final _Action acceptApprovalBlock = new _Action(_ActionType.API_ACTION).id("acceptApprovalBlock").caption("accept").url("acceptApprovalBlock");
+    public static final _Action declineApprovalBlock = new _Action(_ActionType.API_ACTION).id("declineApprovalBlock").caption("decline").url("declineApprovalBlock");
 
     public static final _Action refreshVew = new _Action(_ActionType.RELOAD).id("refresh").icon("fa fa-refresh");
     public static final _Action close = new _Action(_ActionType.CLOSE).caption("close").icon("fa fa-chevron-left").cls("btn-back");
-    public static final _Action saveAndClose = new _Action(_ActionType.SAVE_AND_CLOSE).caption("save_close");
+    public static final _Action saveAndClose = new _Action(_ActionType.SAVE_AND_CLOSE).caption("save_close").cls("btn-primary");
     public static final _Action deleteDocument = new _Action(_ActionType.DELETE_DOCUMENT).caption("delete").cls("btn-warning-effect");
 }
