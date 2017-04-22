14af419087be81d07debe93dde661cf73e8987aa
kaira <biosubj@gmail.com>
2017-04-22 13:11:29 +0300

    #

--- rule/Workflow/Resources/scripts/workflow/dao/ControlledDocumentDAO.java
+++ rule/Workflow/Resources/scripts/workflow/dao/ControlledDocumentDAO.java
@@ -1,17 +1,44 @@
 package workflow.dao;
 
+import java.util.List;
 import java.util.UUID;
 
+import com.exponentus.common.dao.ViewEntryDAO;
+import com.exponentus.common.model.IHierarchicalEntity;
+import com.exponentus.common.model.ViewEntry;
 import com.exponentus.dataengine.exception.DAOException;
 import com.exponentus.dataengine.jpa.DAO;
+import com.exponentus.dataengine.jpa.ViewPage;
+import com.exponentus.runtimeobj.IAppEntity;
+import com.exponentus.scripting.SortParams;
 import com.exponentus.scripting._Session;
 
 import workflow.model.ControlledDocument;
 
-public class ControlledDocumentDAO extends DAO<ControlledDocument, UUID> {
+public class ControlledDocumentDAO<T extends IAppEntity<UUID> & IHierarchicalEntity, K> extends DAO<T, K> {
 
-	public ControlledDocumentDAO(_Session session) throws DAOException {
-		super(ControlledDocument.class, session);
+	public ControlledDocumentDAO(Class<T> entityClass, _Session session) throws DAOException {
+		super(entityClass, session);
+	}
+
+	public ViewPage<T> findAllWithDescendants(SortParams sortParams, int pageNum, int pageSize,
+			List<UUID> expandedIds) {
+		ViewPage<T> vp = findViewPage(sortParams, pageNum, pageSize);
+
+		if (vp.getResult().isEmpty()) {
+			return vp;
+		}
+
+		ViewEntryDAO<ControlledDocument, UUID> veDao = new ViewEntryDAO<ControlledDocument, UUID>(getSession());
+		for (T parent : vp.getResult()) {
+			List<ViewEntry> responses = veDao.findAllDescendants(parent.getId(), expandedIds);
+			if (responses != null && responses.size() > 0) {
+				parent.setResponsesCount((long) responses.size());
+				parent.setResponsesAsViewEntry(responses);
+			}
+		}
+
+		return vp;
 	}
 
 }
