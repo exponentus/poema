70ae7cdf0321ce3bce4779803fdd3b184f3d1f8c
medin84 <medin_84@mail.ru>
2017-04-26 10:15:22 +0600

    commit

--- rule/Workflow/Resources/scripts/workflow/services/AssignmentService.java
+++ rule/Workflow/Resources/scripts/workflow/services/AssignmentService.java
@@ -84,7 +84,7 @@ public class AssignmentService extends RestProvider {
 
             // permissions
             Map<String, Boolean> permissions = new HashMap<>();
-            if (entity.getAppliedAuthor().getId().equals(currentUserEmployee.getId())) {
+            if (!entity.isNew() && entity.getAppliedAuthor().getId().equals(currentUserEmployee.getId())) {
                 permissions.put("RESET_ASSIGNEE", true);
             }
 
