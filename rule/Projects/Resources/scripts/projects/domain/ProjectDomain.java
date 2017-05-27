6fd73a294e77299d3b95d81a11aae0efc30ded65
kaira <biosubj@gmail.com>
2017-05-27 12:28:46 +0300

    #

--- rule/Projects/Resources/scripts/projects/domain/ProjectDomain.java
+++ rule/Projects/Resources/scripts/projects/domain/ProjectDomain.java
@@ -1,80 +1,85 @@
 package projects.domain;
 
-import administrator.model.User;
-import com.exponentus.common.model.ACL;
-import com.exponentus.env.EnvConst;
-import com.exponentus.rest.outgoingdto.Outcome;
-import projects.model.Project;
-import projects.model.constants.ProjectStatusType;
-
 import java.util.HashSet;
 import java.util.Set;
 
+import com.exponentus.common.model.ACL;
+import com.exponentus.env.EnvConst;
+import com.exponentus.rest.outgoingdto.Outcome;
+
+import administrator.model.User;
+import projects.model.Project;
+import projects.model.constants.ProjectStatusType;
+
 public class ProjectDomain {
 
-    public Project composeNew(User author) {
-        Project project = new Project();
+	public Project composeNew(User author) {
+		Project project = new Project();
 
-        project.setAuthor(author);
-        project.setComment("");
-        project.setStatus(ProjectStatusType.DRAFT);
+		project.setAuthor(author);
+		project.setComment("");
+		project.setStatus(ProjectStatusType.DRAFT);
 
-        return project;
-    }
+		return project;
+	}
 
-    public void fillFromDto(Project project, Project dto, User author) {
-        if (project.isNew()) {
-            project.setAuthor(author);
-        }
-        project.setName(dto.getName());
-        project.setCustomer(dto.getCustomer());
-        project.setManager(dto.getManager());
-        project.setProgrammer(dto.getProgrammer());
-        project.setTester(dto.getTester());
-        project.setObservers(dto.getObservers());
-        project.setRepresentatives(dto.getRepresentatives());
-        project.setComment(dto.getComment());
-        project.setStatus(dto.getStatus());
-        project.setFinishDate(dto.getFinishDate());
-        project.setAttachments(dto.getAttachments());
-        project.setPrimaryLanguage(EnvConst.getDefaultLang());
+	public void fillFromDto(Project project, Project dto, User author) {
+		if (project.isNew()) {
+			project.setAuthor(author);
+		}
+		project.setName(dto.getName());
+		project.setCustomer(dto.getCustomer());
+		project.setManager(dto.getManager());
+		project.setProgrammer(dto.getProgrammer());
+		project.setTester(dto.getTester());
+		project.setObservers(dto.getObservers());
+		project.setRepresentatives(dto.getRepresentatives());
+		project.setComment(dto.getComment());
+		project.setStatus(dto.getStatus());
+		project.setFinishDate(dto.getFinishDate());
+		project.setAttachments(dto.getAttachments());
+		project.setPrimaryLanguage(EnvConst.getDefaultLang());
 
-        calculateReaders(project);
-    }
+		calculateReaders(project);
+	}
 
-    public void calculateReaders(Project project) {
-        Set<Long> readers = new HashSet<>();
-        readers.add(project.getAuthor().getId());
-        readers.add(project.getManager());
-        readers.add(project.getProgrammer());
-        if (project.getTester() > 0) {
-            readers.add(project.getTester());
-        }
-        if (project.getObservers() != null) {
-            readers.addAll(project.getObservers());
-        }
+	public void calculateReaders(Project project) {
+		Set<Long> readers = new HashSet<>();
+		try {
+			readers.add(project.getAuthor().getId());
+		} catch (Exception e) {
 
-        project.setReaders(readers);
-    }
+		}
+		readers.add(project.getManager());
+		readers.add(project.getProgrammer());
+		if (project.getTester() > 0) {
+			readers.add(project.getTester());
+		}
+		if (project.getObservers() != null) {
+			readers.addAll(project.getObservers());
+		}
 
-    public boolean projectCanBeDeleted(Project project) {
-        return !project.isNew() && project.isEditable() && project.getStatus() == ProjectStatusType.DRAFT;
-    }
+		project.setReaders(readers);
+	}
 
-    public Outcome getOutcome(Project project) {
-        Outcome outcome = new Outcome();
+	public boolean projectCanBeDeleted(Project project) {
+		return !project.isNew() && project.isEditable() && project.getStatus() == ProjectStatusType.DRAFT;
+	}
 
-        if (project.isNew()) {
-            outcome.setTitle("new_project");
-        } else {
-            outcome.setTitle("project");
-        }
+	public Outcome getOutcome(Project project) {
+		Outcome outcome = new Outcome();
 
-        outcome.addPayload(project);
-        if (!project.isNew()) {
-            outcome.addPayload(new ACL(project));
-        }
+		if (project.isNew()) {
+			outcome.setTitle("new_project");
+		} else {
+			outcome.setTitle("project");
+		}
 
-        return outcome;
-    }
+		outcome.addPayload(project);
+		if (!project.isNew()) {
+			outcome.addPayload(new ACL(project));
+		}
+
+		return outcome;
+	}
 }
