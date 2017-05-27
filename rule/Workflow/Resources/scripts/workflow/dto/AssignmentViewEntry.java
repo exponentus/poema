4ead46981eb8306b19112bd614bbf36ee52d2bbf
medin84 <medin_84@mail.ru>
2017-05-27 23:46:34 +0600

    c

--- rule/Workflow/Resources/scripts/workflow/dto/AssignmentViewEntry.java
+++ rule/Workflow/Resources/scripts/workflow/dto/AssignmentViewEntry.java
@@ -1,35 +1,44 @@
 package workflow.dto;
 
+import com.exponentus.runtimeobj.IAppEntity;
+import reference.model.ControlType;
+import workflow.init.AppConst;
+import workflow.model.constants.ControlStatusType;
+
+import java.util.Date;
 import java.util.List;
 import java.util.UUID;
 
-import com.exponentus.runtimeobj.IAppEntity;
-
-import workflow.init.AppConst;
-
 public class AssignmentViewEntry {
 
-	public UUID id;
-	public String kind = "assignment";
+    public UUID id;
+    public String kind = "assignment";
 
-	public String appliedAuthor;
-	public String body;
-	// public ControlShort control;
+    public String appliedAuthor;
+    public String body;
+    public ControlType controlType;
+    public Date startDate;
+    public Date dueDate;
+    public ControlStatusType status = ControlStatusType.UNKNOWN;
+    public List<AssigneeEntryShort> assigneeEntries;
 
-	public List<IAppEntity<UUID>> responses;
+    public List<IAppEntity<UUID>> responses;
 
-	public AssignmentViewEntry(UUID id, String appliedAuthor, String body) {
-		this.id = id;
-		this.appliedAuthor = appliedAuthor;
-		this.body = body;
-		//this.control = new ControlShort(control);
-	}
+    public AssignmentViewEntry(UUID id, String appliedAuthor, String body, ControlType controlType, Date startDate, Date dueDate, ControlStatusType status) {
+        this.id = id;
+        this.appliedAuthor = appliedAuthor;
+        this.body = body;
+        this.controlType = controlType;
+        this.startDate = startDate;
+        this.dueDate = dueDate;
+        this.status = status;
+    }
 
-	public void setResponses(List<IAppEntity<UUID>> responses) {
-		this.responses = responses;
-	}
+    public void setResponses(List<IAppEntity<UUID>> responses) {
+        this.responses = responses;
+    }
 
-	public String getURL() {
-		return AppConst.BASE_URL + "assignments/" + id;
-	}
+    public String getURL() {
+        return AppConst.BASE_URL + "assignments/" + id;
+    }
 }
