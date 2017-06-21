f92571063685116b1a2f59c1310c7495c0775326
kaira <biosubj@gmail.com>
2017-06-21 18:51:29 +0300

    #

--- rule/Workflow/Resources/scripts/workflow/domain/ReportDomain.java
+++ rule/Workflow/Resources/scripts/workflow/domain/ReportDomain.java
@@ -1,6 +1,10 @@
 package workflow.domain;
 
-import com.exponentus.common.domain.DTOService;
+import java.util.ArrayList;
+import java.util.Date;
+import java.util.List;
+
+import com.exponentus.common.domain.CommonDomain;
 import com.exponentus.common.domain.IValidation;
 import com.exponentus.common.model.ACL;
 import com.exponentus.dataengine.exception.DAOException;
@@ -9,6 +13,7 @@ import com.exponentus.rest.outgoingdto.Outcome;
 import com.exponentus.rest.validation.exception.DTOException;
 import com.exponentus.scripting._Session;
 import com.exponentus.util.StringUtil;
+
 import staff.dao.EmployeeDAO;
 import staff.model.Employee;
 import staff.model.embedded.Observer;
@@ -16,95 +21,91 @@ import workflow.dao.ReportDAO;
 import workflow.model.Assignment;
 import workflow.model.Report;
 
-import java.util.ArrayList;
-import java.util.Date;
-import java.util.List;
+public class ReportDomain extends CommonDomain<Report> {
 
-public class ReportDomain extends DTOService<Report> {
+	public ReportDomain(_Session ses) throws DAOException {
+		super(ses);
+		dao = new ReportDAO(ses);
+	}
 
-    public ReportDomain(_Session ses) throws DAOException {
-        super(ses);
-        dao = new ReportDAO(ses);
-    }
+	public Report composeNew(Employee author, Assignment parent) {
+		if (parent == null) {
+			throw new IllegalArgumentException("parent null");
+		}
 
-    public Report composeNew(Employee author, Assignment parent) {
-        if (parent == null) {
-            throw new IllegalArgumentException("parent null");
-        }
+		Report entity = new Report();
+		entity.setAuthor(author.getUser());
+		entity.setAppliedAuthor(author);
+		entity.setAppliedRegDate(new Date());
+		entity.setParent(parent);
 
-        Report entity = new Report();
-        entity.setAuthor(author.getUser());
-        entity.setAppliedAuthor(author);
-        entity.setAppliedRegDate(new Date());
-        entity.setParent(parent);
+		return entity;
+	}
 
-        return entity;
-    }
+	@Override
+	public Report fillFromDto(Report dto, IValidation<Report> validation, String fsid) throws DTOException, DAOException {
+		validation.check(dto);
 
-    @Override
-    public Report fillFromDto(Report dto, IValidation<Report> validation, String fsid) throws DTOException, DAOException {
-        validation.check(dto);
+		Report entity;
 
-        Report entity;
+		if (dto.isNew()) {
+			entity = new Report();
+		} else {
+			entity = dao.findById(dto.getId());
+		}
 
-        if (dto.isNew()) {
-            entity = new Report();
-        } else {
-            entity = dao.findById(dto.getId());
-        }
+		EmployeeDAO eDao = new EmployeeDAO(ses);
+		if (entity.isNew()) {
+			if (dto.getParent() == null) {
+				throw new DTOException("assignment", "required", "field_is_empty");
+			}
+			entity.setAppliedAuthor(eDao.findById(dto.getAppliedAuthor().getId()));
+			entity.setAppliedRegDate(dto.getAppliedRegDate());
+			entity.setParent(dto.getParent());
+			entity.setAuthor(ses.getUser());
 
-        EmployeeDAO eDao = new EmployeeDAO(ses);
-        if (entity.isNew()) {
-            if (dto.getParent() == null) {
-                throw new DTOException("assignment", "required", "field_is_empty");
-            }
-            entity.setAppliedAuthor(eDao.findById(dto.getAppliedAuthor().getId()));
-            entity.setAppliedRegDate(dto.getAppliedRegDate());
-            entity.setParent(dto.getParent());
-            entity.setAuthor(ses.getUser());
+		}
+		entity.setTitle(dto.getTitle());
+		entity.setBody(dto.getBody());
+		entity.setAppliedAuthor(dto.getAppliedAuthor());
+		entity.setAppliedRegDate(dto.getAppliedRegDate());
 
-        }
-        entity.setTitle(dto.getTitle());
-        entity.setBody(dto.getBody());
-        entity.setAppliedAuthor(dto.getAppliedAuthor());
-        entity.setAppliedRegDate(dto.getAppliedRegDate());
+		List<Observer> observers = new ArrayList<Observer>();
+		for (Observer o : dto.getObservers()) {
+			Observer observer = new Observer();
+			observer.setEmployee(eDao.findById(o.getEmployee().getId()));
+			observers.add(observer);
+		}
+		entity.setObservers(observers);
+		dto.setAttachments(getActualAttachments(entity.getAttachments(), dto.getAttachments(), fsid));
+		calculateReadersEditors(entity);
+		return entity;
+	}
 
-        List<Observer> observers = new ArrayList<Observer>();
-        for (Observer o : dto.getObservers()) {
-            Observer observer = new Observer();
-            observer.setEmployee(eDao.findById(o.getEmployee().getId()));
-            observers.add(observer);
-        }
-        entity.setObservers(observers);
-        dto.setAttachments(getActualAttachments(entity.getAttachments(), dto.getAttachments(), fsid));
-        calculateReadersEditors(entity);
-        return entity;
-    }
+	private void calculateReadersEditors(Report entity) {
+		entity.addReaderEditor(entity.getAuthor());
+	}
 
-    private void calculateReadersEditors(Report entity) {
-        entity.addReaderEditor(entity.getAuthor());
-    }
+	@Override
+	public Report save(Report entity) throws SecureException, DAOException, DTOException {
+		return dao.save(entity);
+	}
 
-    @Override
-    public Report save(Report entity) throws SecureException, DAOException, DTOException {
-        return dao.save(entity);
-    }
+	@Override
+	public Outcome getOutcome(Report entity) {
+		Outcome outcome = new Outcome();
 
-    @Override
-    public Outcome getOutcome(Report entity) {
-        Outcome outcome = new Outcome();
+		if (StringUtil.isEmpty(entity.getTitle())) {
+			outcome.setTitle("report");
+		} else {
+			outcome.setTitle(entity.getTitle());
+		}
+		outcome.addPayload(entity);
+		outcome.addPayload("assignment", entity.getParent());
+		if (!entity.isNew()) {
+			outcome.addPayload(new ACL(entity));
+		}
 
-        if (StringUtil.isEmpty(entity.getTitle())) {
-            outcome.setTitle("report");
-        } else {
-            outcome.setTitle(entity.getTitle());
-        }
-        outcome.addPayload(entity);
-        outcome.addPayload("assignment", entity.getParent());
-        if (!entity.isNew()) {
-            outcome.addPayload(new ACL(entity));
-        }
-
-        return outcome;
-    }
+		return outcome;
+	}
 }
