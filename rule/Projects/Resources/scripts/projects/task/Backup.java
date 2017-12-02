89da92576a39614b277133edf0af8175a062e849
kaira <biosubj@gmail.com>
2017-12-02 21:53:34 +0000

    #

---
+++ rule/Projects/Resources/scripts/projects/task/Backup.java
@@ -0,0 +1,23 @@
+package projects.task;
+
+
+import com.exponentus.appenv.AppEnv;
+import com.exponentus.common.task.AbstractBackup;
+import com.exponentus.scripting._Session;
+import com.exponentus.scriptprocessor.tasks.Command;
+import projects.init.AppConst;
+import projects.model.Project;
+import projects.model.Request;
+import projects.model.Task;
+
+
+//run task prj_backup
+@Command(name = AppConst.CODE + "_backup")
+public class Backup extends AbstractBackup {
+
+    public void doTask(AppEnv appEnv, _Session ses) {
+        backup(appEnv, ses, Project.class, Task.class, Request.class);
+    }
+
+
+}
