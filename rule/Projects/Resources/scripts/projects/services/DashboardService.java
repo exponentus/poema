e8c574027839f43dd0c2766fd80150b3cce72b53
kaira <biosubj@gmail.com>
2017-12-10 19:16:30 +0000

    #

--- rule/Projects/Resources/scripts/projects/services/DashboardService.java
+++ rule/Projects/Resources/scripts/projects/services/DashboardService.java
@@ -5,11 +5,9 @@ import com.exponentus.rest.RestProvider;
 import com.exponentus.rest.outgoingdto.Outcome;
 import com.exponentus.scripting._Session;
 import dataexport.model.constants.ExportFormatType;
-import monitoring.dao.StatisticDAO;
-import org.apache.commons.lang3.time.DateUtils;
+import monitoring.dto.TimeChart;
 import projects.dao.TaskDAO;
 import projects.dto.stat.CountStat;
-import projects.model.constants.TaskStatusType;
 
 import javax.ws.rs.GET;
 import javax.ws.rs.Path;
@@ -32,42 +30,22 @@ public class DashboardService extends RestProvider {
             outcome.setId("dashboard");
             outcome.setTitle("dashboard");
 
-            StatisticDAO monitDao = new StatisticDAO(session);
             Date current = new Date();
             // assignee_state
-            outcome.addPayload("statAssigneeStateProcessing", monitDao.getStatusStat(projects.init.AppConst.CODE, "assignee_state",
-                    session.getUser(),
-                    DateUtils.addMonths(current, -1), current, TaskStatusType.PROCESSING.name()));
-            outcome.addPayload("statAssigneeStateCompleted", monitDao.getStatusStat(projects.init.AppConst.CODE, "assignee_state",
-                    session.getUser(),
-                    DateUtils.addMonths(current, -1), current, TaskStatusType.COMPLETED.name()));
+           outcome.addPayload("statAssigneeStateProcessing", new TimeChart());
+
+            outcome.addPayload("statAssigneeStateCompleted", new TimeChart());
 
             // author_state
-            outcome.addPayload("statAuthorStateProcessing", monitDao.getStatusStat(projects.init.AppConst.CODE, "author_state",
-                    session.getUser(),
-                    DateUtils.addMonths(current, -1), current, TaskStatusType.PROCESSING.name()));
-            outcome.addPayload("statAuthorStateCompleted", monitDao.getStatusStat(projects.init.AppConst.CODE, "author_state",
-                    session.getUser(),
-                    DateUtils.addMonths(current, -1), current, TaskStatusType.COMPLETED.name()));
+            outcome.addPayload("statAuthorStateProcessing", new TimeChart());
+
+            outcome.addPayload("statAuthorStateCompleted", new TimeChart());
 
             TaskDAO taskDAO = new TaskDAO(session);
 
-//            outcome.addPayload("created_by_me", taskDAO.findCreatedByUser(session.getUser(), pageNum, pageSize));
-//            outcome.addPayload("assigned_to_me", taskDAO.findAssignedToUser(session.getUser(), pageNum, pageSize));
-
-//            List<CountStat> taskPriorityStatList = taskDAO.getStatTaskPriority();
             List<CountStat> taskStatusStatList = taskDAO.getStatTaskStatus();
-            // ViewPage tasksDueToday = taskDAO.findAllTaskDueToday();
-            // ViewPage tasksIn7Day = taskDAO.findAllTaskIn7Day();
-            // ViewPage tasksExpired = taskDAO.findAllTaskExpired();
-
-//            outcome.addPayload("taskPriorityStat", taskPriorityStatList);
             outcome.addPayload("taskStatusStat", taskStatusStatList);
             outcome.addPayload("exportFormatType", ExportFormatType.values());
-
-            //   outcome.addPayload("tasksDueToday", tasksDueToday);
-            //   outcome.addPayload("tasksIn7Day", tasksIn7Day);
-            //   outcome.addPayload("tasksExpired", tasksExpired);
 
             return Response.ok(outcome).build();
         } catch (DAOException e) {
