b5e66babd2c2f027dfbaba4267810830b14e408e
medin84 <medin_84@mail.ru>
2018-01-31 18:22:33 +0600

    c

--- webapps/ROOT/sw.js
+++ webapps/ROOT/sw.js
@@ -14,11 +14,7 @@ const networkFirst = workboxSW.strategies.networkFirst({
 
 // cacheFirst
 workboxSW.router.registerRoute(new RegExp('^' + host + '/(.*?).js'), cacheFirst);
-
-workboxSW.router.registerRoute('/Workspace/manifest.json', cacheFirst);
-workboxSW.router.registerRoute('/Workspace/', cacheFirst);
 workboxSW.router.registerRoute(new RegExp('^' + host + '/(\\w+)/img/*'), cacheFirst);
-workboxSW.router.registerRoute(new RegExp('^' + host + '/(\\w+)/i18n/(\\w+).json'), cacheFirst);
 workboxSW.router.registerRoute(new RegExp('^' + host + '/SharedResources/*'), cacheFirst);
 workboxSW.router.registerRoute(new RegExp('^' + host + '/Staff/api/employees/(.*?)/avatar\\?_thumbnail'), cacheFirst);
 
@@ -26,16 +22,19 @@ workboxSW.router.registerRoute(new RegExp('^' + host + '/Staff/api/employees/(.*
 workboxSW.router.registerRoute(new RegExp('^https://fonts.googleapis.com/*'), cacheFirst);
 
 // networkFirst
+workboxSW.router.registerRoute('/Workspace/', networkFirst);
+workboxSW.router.registerRoute('/Workspace/manifest.json', networkFirst);
 workboxSW.router.registerRoute(new RegExp('^' + host + '/(\\w+)/api/session'), networkFirst);
+workboxSW.router.registerRoute(new RegExp('^' + host + '/(\\w+)/i18n/(\\w+).json'), networkFirst);
 
 // module api url regexp
 const moduleApiUrlRegexp = new RegExp('^' + host + '/(\\w+)/api/*');
-const OFFLINE_API_JSON = {
-    id: 'OFFLINE',
-    title: 'internet_offline',
-    message: 'internet_offline',
-    payload: {}
-};
+// const OFFLINE_API_JSON = {
+//     id: 'OFFLINE',
+//     title: 'internet_offline',
+//     message: 'internet_offline',
+//     payload: {}
+// };
 
 // handle fetch
 self.addEventListener('fetch', event => {
@@ -48,6 +47,10 @@ self.addEventListener('fetch', event => {
                     }
                     return networkResponse;
                 });
+
+                if (self.navigator.onLine) {
+                    return fetchPromise;
+                }
                 return cacheResponse || fetchPromise;
                 //
                 // if (cacheResponse) {
