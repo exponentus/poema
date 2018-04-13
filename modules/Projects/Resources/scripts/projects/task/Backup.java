f71fb22d2a2c4086e09d2f5dda49bd86268464a7
kaira <biosubj@gmail.com>
2018-04-13 21:05:34 +0100

    #

--- modules/Projects/Resources/scripts/projects/task/Backup.java
+++ modules/Projects/Resources/scripts/projects/task/Backup.java
@@ -3,12 +3,18 @@ package projects.task;
 
 import com.exponentus.appenv.AppEnv;
 import com.exponentus.common.task.AbstractBackup;
+import com.exponentus.env.EnvConst;
 import com.exponentus.scripting._Session;
 import com.exponentus.scriptprocessor.tasks.Command;
+import com.exponentus.util.FileUtil;
+import com.exponentus.util.TimeUtil;
 import projects.init.ModuleConst;
 import projects.model.Project;
 import projects.model.Request;
 import projects.model.Task;
+
+import java.io.File;
+import java.util.Date;
 
 
 //run task prj_backup
@@ -16,8 +22,18 @@ import projects.model.Task;
 public class Backup extends AbstractBackup {
 
     public void doTask(AppEnv appEnv, _Session ses) {
-        backup(appEnv, ses, Project.class, Task.class, Request.class);
+        Class[] classesToBackup = {
+                Project.class,
+                Task.class,
+                Request.class,
+                };
+
+        for (Class clazz : classesToBackup) {
+            String folder = backup(appEnv, ses, clazz);
+            String simpleName = clazz.getSimpleName();
+            String zipFileName = EnvConst.BACKUP_DIR + File.separator + appEnv.appName + "_" +
+                    simpleName + "_" + TimeUtil.dateTimeToFileNameSilently(new Date()) + ".zip";
+            FileUtil.zipFolder(folder, zipFileName, true);
+        }
     }
-
-
 }
