1c871d5c0901a825c8b04d21c58c8d4701d8ac39
medin84 <medin_84@mail.ru>
2018-05-14 14:07:19 +0600

    index page

--- modules/Projects/Resources/scripts/projects/page/Index.java
+++ modules/Projects/Resources/scripts/projects/page/Index.java
@@ -4,7 +4,6 @@ import com.exponentus.env.Environment;
 import com.exponentus.scripting.WebFormData;
 import com.exponentus.scripting._Session;
 import com.exponentus.scripting.event._DoPage;
-import com.exponentus.user.AnonymousUser;
 
 public class Index extends _DoPage {
 
@@ -12,11 +11,5 @@ public class Index extends _DoPage {
     public void doGET(_Session session, WebFormData formData) {
         addValue("logo", Environment.logo);
         addValue("locale", session.getLang().getAlternateCode());
-
-        if (!formData.containsField("skip-auth-error")
-                && !formData.containsField("pwa")
-                && session.getUser().getUserID().equalsIgnoreCase(AnonymousUser.USER_NAME)) {
-            setUnauthorized();
-        }
     }
 }
