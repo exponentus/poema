8563bfb2ad2a4c7e70ae281776cacd7ea305ef5c
medin84 <medin_84@mail.ru>
2018-11-30 13:36:34 +0600

    c

--- webapps/Projects/ng-app/src/app/projects/models/task.ts
+++ webapps/Projects/ng-app/src/app/projects/models/task.ts
@@ -22,6 +22,7 @@ export class Task extends BaseModel {
     assigneeUserId: string;
     startDate: Date;
     dueDate: Date;
+    plannedTimeInHours: number;
     initiative: boolean;
     tags: Tag[];
     observers: IEntity[];
